2c50b65764df28b7591b785025583abae8218b72
Ben Allenden <benallenden@protonmail.com>
2025-03-08 16:58:47 +0000

    Update ImageValidator methods to static references

--- src/main/java/com/example/echo_api/util/validator/ImageValidator.java
+++ src/main/java/com/example/echo_api/util/validator/ImageValidator.java
@@ -1,5 +1,7 @@
 package com.example.echo_api.util.validator;
 
+import static lombok.AccessLevel.PRIVATE;
+
 import java.util.List;
 
 import org.springframework.lang.NonNull;
@@ -8,6 +10,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.example.echo_api.exception.custom.file.ImageFormatException;
 import com.example.echo_api.exception.custom.file.ImageSizeException;
+
+import lombok.NoArgsConstructor;
+
 import com.example.echo_api.exception.custom.file.FileInvalidException;
 
 /**
@@ -15,17 +20,18 @@ import com.example.echo_api.exception.custom.file.FileInvalidException;
  * uploaded images meet size and type requirements.
  */
 @Component
+@NoArgsConstructor(access = PRIVATE)
 public class ImageValidator {
 
     private static final long MAX_SIZE_MB = 2;
     private static final long MAX_SIZE_BYTES = MAX_SIZE_MB * 1024 * 1024;
     private static final List<String> ALLOWED_TYPES = List.of("image/jpeg", "image/png");
 
-    public boolean supports(@NonNull Class<?> clazz) {
+    public static boolean supports(@NonNull Class<?> clazz) {
         return MultipartFile.class.equals(clazz);
     }
 
-    public void validate(@NonNull Object target) {
+    public static void validate(@NonNull Object target) {
         MultipartFile file = (MultipartFile) target;
 
         if (file.isEmpty()) {
