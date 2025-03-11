ff2803c3b9a2f5aafb87580624155fcd98aa95ee
Ben Allenden <benallenden@protonmail.com>
2025-03-11 15:29:55 +0000

    Add ProfileService update/delete avatar/banner methods

--- src/main/java/com/example/echo_api/service/profile/ProfileService.java
+++ src/main/java/com/example/echo_api/service/profile/ProfileService.java
@@ -3,7 +3,10 @@ package com.example.echo_api.service.profile;
 import java.util.List;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.example.echo_api.exception.custom.cloudinary.CloudinaryException;
+import com.example.echo_api.exception.custom.image.ImageException;
 import com.example.echo_api.exception.custom.username.UsernameNotFoundException;
 import com.example.echo_api.persistence.dto.request.profile.UpdateProfileDTO;
 import com.example.echo_api.persistence.dto.response.pagination.PageDTO;
@@ -40,6 +43,50 @@ public interface ProfileService {
      *                profile information.
      */
     public void updateMeProfile(UpdateProfileDTO request);
+
+    /**
+     * Updates the profile avatar image of the authenticated user.
+     * 
+     * @param image The {@link MultipartFile} resembling the uploaded image.
+     * @throws ImageException      If there is an existing avatar and its
+     *                             {@link Image} entity could not be found when
+     *                             deleting.
+     * @throws CloudinaryException If there was an error when interacting with the
+     *                             Cloudinary SDK.
+     */
+    public void updateMeAvatar(MultipartFile image) throws ImageException, CloudinaryException;
+
+    /**
+     * Removes the profile avatar image of the authenticated user.
+     * 
+     * @throws ImageException      If the existing avatar's {@link Image} entity
+     *                             could not be found when deleting.
+     * @throws CloudinaryException If there was an error when interacting with the
+     *                             Cloudinary SDK.
+     */
+    public void deleteMeAvatar() throws ImageException, CloudinaryException;
+
+    /**
+     * Updates the profile banner image of the authenticated user.
+     * 
+     * @param image The {@link MultipartFile} resembling the uploaded image.
+     * @throws ImageException      If there is an existing banner and its
+     *                             {@link Image} entity could not be found when
+     *                             deleting.
+     * @throws CloudinaryException If there was an error when interacting with the
+     *                             Cloudinary SDK.
+     */
+    public void updateMeBanner(MultipartFile image) throws ImageException, CloudinaryException;
+
+    /**
+     * Removes the profile avatar image of the authenticated user.
+     * 
+     * @throws ImageException      If the existing banner's {@link Image} entity
+     *                             could not be found when deleting.
+     * @throws CloudinaryException If there was an error when interacting with the
+     *                             Cloudinary SDK.
+     */
+    public void deleteMeBanner() throws ImageException, CloudinaryException;
 
     /**
      * Fetches a {@link List} of {@link Profile} for the followers list of the
