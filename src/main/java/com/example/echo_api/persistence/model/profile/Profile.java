1ab3508afb0f25b07a7775d44ee934df62ed9d27
Ben Allenden <benallenden@protonmail.com>
2025-04-02 18:46:25 +0100

    Add additional field validation

--- src/main/java/com/example/echo_api/persistence/model/profile/Profile.java
+++ src/main/java/com/example/echo_api/persistence/model/profile/Profile.java
@@ -12,6 +12,7 @@ import com.example.echo_api.validation.account.annotations.Username;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
@@ -36,26 +37,29 @@ public class Profile {
     @Column(unique = true, nullable = false)
     private String username;
 
+    @Column(length = 50)
     private String name;
 
+    @Column(length = 160)
     private String bio;
 
+    @Column(length = 30)
     private String location;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "avatar_id")
     private Image avatar;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "banner_id")
     private Image banner;
 
     @CreationTimestamp
-    @Column(name = "created_at")
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @UpdateTimestamp
-    @Column(name = "updated_at")
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
     // ---- constructors ----
