51c58c9b1c48ae9846aa1d8060168ef0c6dfa4e9
Ben Allenden <benallenden@protonmail.com>
2025-04-15 19:35:20 +0100

    Repository interface for custom methods to efficiently query and build profile DTOs

---
+++ src/main/java/com/example/echo_api/persistence/repository/custom/CustomProfileRepository.java
@@ -0,0 +1,60 @@
+package com.example.echo_api.persistence.repository.custom;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.example.echo_api.persistence.dto.response.profile.ProfileDTO;
+
+public interface CustomProfileRepository {
+
+    /**
+     * Find the necessary information to build a {@link ProfileDTO} by {@code id}.
+     * 
+     * @param id                  The id to search within the repository.
+     * @param authenticatedUserId The id of the authenticated user.
+     * @return An {@link Optional} containing a {@link ProfileDTO} if found, else
+     *         empty.
+     */
+    Optional<ProfileDTO> findProfileDtoForId(UUID id, UUID authenticatedUserId);
+
+    /**
+     * Find the necessary information to build a {@link ProfileDTO} by
+     * {@code username}.
+     * 
+     * @param username            The username to search within the repository.
+     * @param authenticatedUserId The id of the authenticated user.
+     * @return An {@link Optional} containing a {@link ProfileDTO} if found, else
+     *         empty.
+     */
+    Optional<ProfileDTO> findProfileDtoForUsername(String username, UUID authenticatedUserId);
+
+    /**
+     * Find the necessary information to build a {@link Page} of {@link ProfileDTO}
+     * for each user following the queried profile by {@code id}.
+     * 
+     * @param id                  The id of the target profile to search within the
+     *                            repository.
+     * @param authenticatedUserId The id of the authenticated user.
+     * @param p                   The pagination config for the query.
+     * @return A {@link Page} of {@link ProfileDTO} ordered by follow date (newest
+     *         first).
+     */
+    Page<ProfileDTO> findAllFollowersProfileDtoForId(UUID id, UUID authenticatedUserId, Pageable p);
+
+    /**
+     * Find the necessary information to build a {@link Page} of {@link ProfileDTO}
+     * for each user followed by the queried profile by {@code id}.
+     * 
+     * @param id                  The id of the target profile to search within the
+     *                            repository.
+     * @param authenticatedUserId The id of the authenticated user.
+     * @param p                   The pagination config for the query.
+     * @return A {@link Page} of {@link ProfileDTO} ordered by follow date (newest
+     *         first).
+     */
+    Page<ProfileDTO> findAllFollowingProfileDtoForId(UUID id, UUID authenticatedUserId, Pageable p);
+
+}
