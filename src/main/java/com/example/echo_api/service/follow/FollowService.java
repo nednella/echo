6948502cd306e7e063cb227288b98e6116f91c3c
Ben Allenden <benallenden@protonmail.com>
2025-04-27 23:22:30 +0100

    Refactor FollowService - delete redundant methods, update Jdocs and add mutualFollowIfExists

--- src/main/java/com/example/echo_api/service/follow/FollowService.java
+++ src/main/java/com/example/echo_api/service/follow/FollowService.java
@@ -1,8 +1,9 @@
 package com.example.echo_api.service.follow;
 
+import java.util.UUID;
+
 import com.example.echo_api.exception.custom.relationship.AlreadyFollowingException;
 import com.example.echo_api.exception.custom.relationship.NotFollowingException;
-import com.example.echo_api.exception.custom.relationship.SelfActionException;
 import com.example.echo_api.persistence.model.follow.Follow;
 import com.example.echo_api.persistence.model.profile.Profile;
 
@@ -10,48 +11,33 @@ public interface FollowService {
 
     /**
      * Creates a {@link Follow} relationship between {@code source} and
-     * {@code target} profiles.
+     * {@code target} profiles. The order of the supplied arguments does matter.
      * 
-     * @param source The initiating {@link Profile}.
-     * @param target The target {@link Profile}.
-     * @throws SelfActionException       If the {@code source} and {@code target}
-     *                                   profiles point to the same user.
+     * @param source The id of the initiating {@link Profile}.
+     * @param target The id of the target {@link Profile}.
      * @throws AlreadyFollowingException If the {@code source} profile already
      *                                   follows the {@code target} profile.
      */
-    public void follow(Profile source, Profile target) throws SelfActionException, AlreadyFollowingException;
+    public void follow(UUID source, UUID target) throws AlreadyFollowingException;
 
     /**
      * Deletes a {@link Follow} relationship between {@code source} and
-     * {@code target} profiles.
+     * {@code target} profiles. The order of the supplied arguments does matter.
      * 
-     * @param source The initiating {@link Profile}.
-     * @param target The target {@link Profile}.
-     * @throws SelfActionException   If the {@code source} and {@code target}
-     *                               profiles point to the same user.
+     * @param source The id of the initiating {@link Profile}.
+     * @param target The id of the target {@link Profile}.
      * @throws NotFollowingException If the {@code source} profile doesn't already
      *                               follow the {@code target} profile.
      */
-    public void unfollow(Profile source, Profile target) throws SelfActionException, NotFollowingException;
+    public void unfollow(UUID source, UUID target) throws NotFollowingException;
 
     /**
-     * Checks if a {@link Follow} relationship exists between {@code source} and
-     * {@code target} profiles.
+     * Delete any follow relationship that exists between two profiles in either
+     * direction (unilateral or mutual).
      * 
-     * @param source The initiating {@link Profile}.
-     * @param target The target {@link Profile}.
-     * @return
+     * @param profileId1 The id of the first profile.
+     * @param profileId2 The id of the second profile.
      */
-    public boolean isFollowing(Profile source, Profile target);
-
-    /**
-     * Checks if a {@link Follow} relationship exists between {@code target} and
-     * {@code source} profiles.
-     * 
-     * @param source The initiating {@link Profile}.
-     * @param target The target {@link Profile}.
-     * @return
-     */
-    public boolean isFollowedBy(Profile source, Profile target);
+    public void mutualUnfollowIfExists(UUID profileId1, UUID profileId2);
 
 }
--- src/main/java/com/example/echo_api/service/follow/FollowServiceImpl.java
+++ src/main/java/com/example/echo_api/service/follow/FollowServiceImpl.java
@@ -1,15 +1,13 @@
 package com.example.echo_api.service.follow;
 
-import java.util.Objects;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.echo_api.exception.custom.relationship.AlreadyFollowingException;
 import com.example.echo_api.exception.custom.relationship.NotFollowingException;
-import com.example.echo_api.exception.custom.relationship.SelfActionException;
 import com.example.echo_api.persistence.model.follow.Follow;
-import com.example.echo_api.persistence.model.profile.Profile;
 import com.example.echo_api.persistence.repository.FollowRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -28,11 +26,7 @@ public class FollowServiceImpl implements FollowService {
     private final FollowRepository followRepository;
 
     @Override
-    @Transactional
-    public void follow(Profile source, Profile target) throws SelfActionException, AlreadyFollowingException {
-        if (isSelfAction(source, target)) {
-            throw new SelfActionException();
-        }
+    public void follow(UUID source, UUID target) throws AlreadyFollowingException {
         if (isFollowing(source, target)) {
             throw new AlreadyFollowingException();
         }
@@ -41,11 +35,7 @@ public class FollowServiceImpl implements FollowService {
     }
 
     @Override
-    @Transactional
-    public void unfollow(Profile source, Profile target) throws SelfActionException, NotFollowingException {
-        if (isSelfAction(source, target)) {
-            throw new SelfActionException();
-        }
+    public void unfollow(UUID source, UUID target) throws NotFollowingException {
         if (!isFollowing(source, target)) {
             throw new NotFollowingException();
         }
@@ -54,24 +44,22 @@ public class FollowServiceImpl implements FollowService {
     }
 
     @Override
-    public boolean isFollowing(Profile source, Profile target) {
-        return followRepository.existsByFollowerIdAndFollowingId(source.getId(), target.getId());
-    }
-
-    @Override
-    public boolean isFollowedBy(Profile source, Profile target) {
-        return isFollowing(target, source);
+    @Transactional
+    public void mutualUnfollowIfExists(UUID profileId1, UUID profileId2) {
+        followRepository.deleteAnyFollowIfExists(profileId1, profileId2);
     }
 
     /**
-     * Internal method for checking if {@link Profile} pairs match.
+     * Internal method to check if a one-way follow exists from the follower to the
+     * followed profile.
      * 
-     * @param source The source {@link Profile}.
-     * @param target The target {@link Profile}.
-     * @return Boolean indicating whether the profiles are a match.
+     * @param source The id of the profile initiating the follow.
+     * @param target The id of the profile being followed.
+     * @return True if a unidirectional follow exists from source to target, else
+     *         false.
      */
-    private boolean isSelfAction(Profile source, Profile target) {
-        return Objects.equals(source.getId(), target.getId());
+    private boolean isFollowing(UUID source, UUID target) {
+        return followRepository.existsByFollowerIdAndFollowingId(source, target);
     }
 
 }
