d589fbe125f2e40c25bf3e03c1610eee4f0df273
Ben Allenden <benallenden@protonmail.com>
2025-04-28 02:01:48 +0100

    Update ProfileController to handle refactored profile services

--- src/main/java/com/example/echo_api/controller/profile/ProfileController.java
+++ src/main/java/com/example/echo_api/controller/profile/ProfileController.java
@@ -10,7 +10,10 @@ import com.example.echo_api.config.ApiConfig;
 import com.example.echo_api.persistence.dto.request.profile.UpdateProfileDTO;
 import com.example.echo_api.persistence.dto.response.pagination.PageDTO;
 import com.example.echo_api.persistence.dto.response.profile.ProfileDTO;
-import com.example.echo_api.service.profile.ProfileService;
+import com.example.echo_api.persistence.dto.response.profile.SimplifiedProfileDTO;
+import com.example.echo_api.service.profile.interaction.ProfileInteractionService;
+import com.example.echo_api.service.profile.management.ProfileManagementService;
+import com.example.echo_api.service.profile.view.ProfileViewService;
 import com.example.echo_api.util.pagination.OffsetLimitRequest;
 import com.example.echo_api.util.validator.ImageValidator;
 import com.example.echo_api.validation.pagination.annotations.Limit;
@@ -33,75 +36,77 @@ import org.springframework.web.bind.annotation.RequestPart;
 @Validated
 public class ProfileController {
 
-    private final ProfileService profileService;
+    private final ProfileViewService profileViewService;
+    private final ProfileManagementService profileManagementService;
+    private final ProfileInteractionService profileInteractionService;
+
+    // ---- get profile ----
 
     @GetMapping(ApiConfig.Profile.ME)
     public ResponseEntity<ProfileDTO> getMe() {
-        ProfileDTO response = profileService.getMe();
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(profileViewService.getSelf());
     }
+
+    @GetMapping(ApiConfig.Profile.GET_BY_USERNAME)
+    public ResponseEntity<ProfileDTO> getByUsername(@PathVariable("username") String username) {
+        return ResponseEntity.ok(profileViewService.getByUsername(username));
+    }
+
+    // ---- update self profile ----
 
     @PutMapping(ApiConfig.Profile.ME_INFO)
     public ResponseEntity<Void> updateMeProfile(@RequestBody @Valid UpdateProfileDTO request) {
-        profileService.updateMeProfile(request);
+        profileManagementService.updateInformation(request);
         return ResponseEntity.noContent().build();
     }
 
     @PostMapping(ApiConfig.Profile.ME_AVATAR)
     public ResponseEntity<Void> updateMeAvatar(@RequestPart("file") MultipartFile file) {
         ImageValidator.validate(file);
-        profileService.updateMeAvatar(file);
+        profileManagementService.updateAvatar(file);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping(ApiConfig.Profile.ME_AVATAR)
     public ResponseEntity<Void> deleteMeAvatar() {
-        profileService.deleteMeAvatar();
+        profileManagementService.deleteAvatar();
         return ResponseEntity.noContent().build();
     }
 
     @PostMapping(ApiConfig.Profile.ME_BANNER)
     public ResponseEntity<Void> updateMeBanner(@RequestPart("file") MultipartFile file) {
         ImageValidator.validate(file);
-        profileService.updateMeBanner(file);
+        profileManagementService.updateBanner(file);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping(ApiConfig.Profile.ME_BANNER)
     public ResponseEntity<Void> deleteMeBanner() {
-        profileService.deleteMeBanner();
+        profileManagementService.deleteBanner();
         return ResponseEntity.noContent().build();
-    }
-
-    @GetMapping(ApiConfig.Profile.GET_BY_USERNAME)
-    public ResponseEntity<ProfileDTO> getByUsername(@PathVariable("username") String username) {
-        ProfileDTO response = profileService.getByUsername(username);
-        return ResponseEntity.ok(response);
     }
 
     // --- following/follower list ----
 
     // @formatter:off
     @GetMapping(ApiConfig.Profile.GET_FOLLOWERS_BY_USERNAME)
-    public ResponseEntity<PageDTO<ProfileDTO>> getFollowers(
+    public ResponseEntity<PageDTO<SimplifiedProfileDTO>> getFollowers(
         @PathVariable("username") String username,
         @RequestParam(name = "offset", defaultValue = "0") @Offset int offset,
         @RequestParam(name = "limit", defaultValue = "20") @Limit int limit
     ) {
         Pageable page = new OffsetLimitRequest(offset, limit);
-        PageDTO<ProfileDTO> response = profileService.getFollowers(username, page);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(profileViewService.getFollowers(username, page));
     }
 
     @GetMapping(ApiConfig.Profile.GET_FOLLOWING_BY_USERNAME)
-    public ResponseEntity<PageDTO<ProfileDTO>> getFollowing(
+    public ResponseEntity<PageDTO<SimplifiedProfileDTO>> getFollowing(
         @PathVariable("username") String username,
         @RequestParam(name = "offset", defaultValue = "0") @Offset int offset,
         @RequestParam(name = "limit", defaultValue = "20") @Limit int limit
     ) {
         Pageable page = new OffsetLimitRequest(offset, limit);
-        PageDTO<ProfileDTO> response = profileService.getFollowing(username, page);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(profileViewService.getFollowing(username, page));
     }
     // @formatter:on
 
@@ -109,13 +114,13 @@ public class ProfileController {
 
     @PostMapping(ApiConfig.Profile.FOLLOW_BY_USERNAME)
     public ResponseEntity<Void> followProfile(@PathVariable("username") String username) {
-        profileService.follow(username);
+        profileInteractionService.follow(username);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping(ApiConfig.Profile.UNFOLLOW_BY_USERNAME)
     public ResponseEntity<Void> unfollowProfile(@PathVariable("username") String username) {
-        profileService.unfollow(username);
+        profileInteractionService.unfollow(username);
         return ResponseEntity.noContent().build();
     }
 
@@ -123,13 +128,13 @@ public class ProfileController {
 
     @PostMapping(ApiConfig.Profile.BLOCK_BY_USERNAME)
     public ResponseEntity<Void> blockProfile(@PathVariable("username") String username) {
-        profileService.block(username);
+        profileInteractionService.block(username);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping(ApiConfig.Profile.UNBLOCK_BY_USERNAME)
     public ResponseEntity<Void> unblockProfile(@PathVariable("username") String username) {
-        profileService.unblock(username);
+        profileInteractionService.unblock(username);
         return ResponseEntity.noContent().build();
     }
 
