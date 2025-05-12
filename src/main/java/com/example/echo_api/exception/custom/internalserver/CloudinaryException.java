012f6bc67c6f237e846d7ac35cb40bb71e563a67
Ben Allenden <benallenden@protonmail.com>
2025-05-12 23:13:43 +0100

    Fix CloudinaryException constructor

--- src/main/java/com/example/echo_api/exception/custom/internalserver/CloudinaryException.java
+++ src/main/java/com/example/echo_api/exception/custom/internalserver/CloudinaryException.java
@@ -1,5 +1,8 @@
 package com.example.echo_api.exception.custom.internalserver;
 
+import com.cloudinary.Cloudinary;
+import com.example.echo_api.config.ErrorMessageConfig;
+
 /**
  * Thrown if there is an error during an operation using the {@link Cloudinary}
  * SDK.
@@ -7,12 +10,12 @@ package com.example.echo_api.exception.custom.internalserver;
 public class CloudinaryException extends InternalServerException {
 
     /**
-     * Constructs a {@link CloudinaryException} with the specified {@code message}.
+     * Constructs a {@link CloudinaryException} with the specified {@code details}.
      * 
-     * @param message The general error message.
+     * @param details The specific error details.
      */
-    public CloudinaryException(String message) {
-        super(message);
+    public CloudinaryException(String details) {
+        super(ErrorMessageConfig.InternalServerError.CLOUDINARY_SDK_ERROR, details);
     }
 
 }
