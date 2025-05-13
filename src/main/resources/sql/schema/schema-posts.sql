a4c193d7c66259c104911e15a779b6072c355866
Ben Allenden <benallenden@protonmail.com>
2025-05-13 21:05:23 +0100

    Fix post table index to non-unique

--- src/main/resources/sql/schema/schema-posts.sql
+++ src/main/resources/sql/schema/schema-posts.sql
@@ -9,22 +9,22 @@ CREATE TABLE
         CONSTRAINT fk_author_id FOREIGN KEY (author_id) REFERENCES "profile"(id)
     );
 
-CREATE UNIQUE INDEX
+CREATE INDEX
     IF NOT EXISTS idx_post_parent_id
         ON "post"(parent_id);
 
-CREATE UNIQUE INDEX
+CREATE INDEX
     IF NOT EXISTS idx_post_parent_id_created_at
         ON "post"(parent_id, created_at);
 
-CREATE UNIQUE INDEX
+CREATE INDEX
     IF NOT EXISTS idx_post_author_id
         ON "post"(author_id);
 
-CREATE UNIQUE INDEX
+CREATE INDEX
     IF NOT EXISTS idx_post_author_id_created_at
         ON "post"(author_id, created_at);
 
-CREATE UNIQUE INDEX
+CREATE INDEX
     IF NOT EXISTS idx_post_created_at
         ON "post"(created_at);
