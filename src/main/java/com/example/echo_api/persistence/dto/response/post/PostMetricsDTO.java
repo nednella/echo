d1d68526c804ccf58174443fe550127946c872ae
Ben Allenden <benallenden@protonmail.com>
2025-05-30 20:38:17 +0100

    Update PostDTO jdocs

--- src/main/java/com/example/echo_api/persistence/dto/response/post/PostMetricsDTO.java
+++ src/main/java/com/example/echo_api/persistence/dto/response/post/PostMetricsDTO.java
@@ -1,11 +1,11 @@
 package com.example.echo_api.persistence.dto.response.post;
 
 /**
- * Represents a standardised response format for post metrics.
+ * Represents a standardised response format for post engagement metrics.
  *
- * @param likes   The number of likes this post has.
- * @param replies The number of replies this post has.
- * @param shares  The number of shares this post has.
+ * @param likes   The number of likes the post has.
+ * @param replies The number of replies the post has.
+ * @param shares  The number of shares the post has.
  */
 // @formatter:off
 public record PostMetricsDTO(
