7bec0ab2ba4b454629f2631a9250c257a8972f6d
Ben Allenden <benallenden@protonmail.com>
2025-06-10 16:13:07 +0100

    Add USERNAME_MENTION and HASHTAG regex patterns for parsing posts on creation

--- src/main/java/com/example/echo_api/config/RegexConfig.java
+++ src/main/java/com/example/echo_api/config/RegexConfig.java
@@ -10,8 +10,7 @@ public class RegexConfig {
      * Username regex pattern
      * 
      * <ul>
-     * <li>Contains only word characters (alphanumerics and underscores)
-     * <li>Contains between 3 and 15 characters
+     * <li>Contains between 3 and 15 word characters
      * </ul>
      */
     public static final String USERNAME = "^\\w{3,15}$";
@@ -22,10 +21,35 @@ public class RegexConfig {
      * <ul>
      * <li>Contains at least one letter
      * <li>Contains at least one number
-     * <li>Contain at least 6 non-whitespace characters (excludes space, tabs and
-     * line breaks)
+     * <li>Contain at least 6 non-whitespace characters
      * </ul>
      */
     public static final String PASSWORD = "^(?=.*[a-zA-Z])(?=.*\\d)[\\S]{6,}$";
+
+    /**
+     * Username mention regex pattern
+     * 
+     * <ul>
+     * <li>Preceded by the start of a string OR a whitespace character
+     * <li>Starts with "@"
+     * <li>Contains between 3 and 15 word characters
+     * <li>Not followed by "@"
+     * <li>Followed by a word boundary character
+     * </ul>
+     */
+    public static final String USERNAME_MENTION = "(?<=^|\\s)@\\w{3,15}(?![@])\\b";
+
+    /**
+     * Hashtag regex pattern
+     * 
+     * <ul>
+     * <li>Preceded by the start of a string OR a whitespace character
+     * <li>Starts with "#"
+     * <li>Contains any word characters, matched 1+ times
+     * <li>Not followed by "#"
+     * <li>Followed by a word boundary character
+     * </ul>
+     */
+    public static final String HASHTAG = "(?<=^|\\s)#\\w+(?![#])\\b";
 
 }
