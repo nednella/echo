87e2723d5457c71937b058cce3e8fdd1e836ec8d
Ben Allenden <benallenden@protonmail.com>
2025-06-10 19:42:53 +0100

    Implement PostEntity persistence logic during the creation of a new post

--- src/main/java/com/example/echo_api/service/post/management/PostManagementServiceImpl.java
+++ src/main/java/com/example/echo_api/service/post/management/PostManagementServiceImpl.java
@@ -1,17 +1,24 @@
 package com.example.echo_api.service.post.management;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.echo_api.exception.custom.badrequest.InvalidParentIdException;
 import com.example.echo_api.exception.custom.forbidden.ResourceOwnershipException;
 import com.example.echo_api.persistence.dto.request.post.CreatePostDTO;
 import com.example.echo_api.persistence.model.post.Post;
+import com.example.echo_api.persistence.model.post.entity.PostEntity;
+import com.example.echo_api.persistence.repository.PostHashtagRepository;
+import com.example.echo_api.persistence.repository.PostMentionRepository;
 import com.example.echo_api.persistence.repository.PostRepository;
 import com.example.echo_api.service.post.BasePostService;
+import com.example.echo_api.service.post.util.PostParsingService;
 import com.example.echo_api.service.session.SessionService;
 
 /**
@@ -21,25 +28,40 @@ import com.example.echo_api.service.session.SessionService;
 @Service
 public class PostManagementServiceImpl extends BasePostService implements PostManagementService {
 
+    private final PostParsingService postParsingService;
+    private final PostHashtagRepository postHashtagRepository;
+    private final PostMentionRepository postMentionRepository;
+
     // @formatter:off
     protected PostManagementServiceImpl(
         SessionService sessionService,
-        PostRepository postRepository) {
+        PostRepository postRepository,
+        PostParsingService postParsingService,
+        PostHashtagRepository postHashtagRepository,
+        PostMentionRepository postMentionRepository) {
         super(sessionService, postRepository);
+        this.postParsingService = postParsingService;
+        this.postHashtagRepository = postHashtagRepository;
+        this.postMentionRepository = postMentionRepository;
     }
     // @formatter:on
 
     @Override
+    @Transactional
     public void create(CreatePostDTO request) {
         UUID parentId = request.parentId() != null ? request.parentId() : null;
         UUID authorId = getAuthenticatedUser().getId();
         String text = request.text();
 
         validatePostExistsByParentId(parentId);
-        postRepository.save(new Post(parentId, authorId, text));
+        Post post = postRepository.save(new Post(parentId, authorId, text));
+
+        Map<String, List<PostEntity>> entities = postParsingService.parse(post.getId(), post.getText());
+        savePostEntities(entities);
     }
 
     @Override
+    @Transactional
     public void delete(UUID id) {
         UUID authenticatedUserId = getAuthenticatedUser().getId();
         Optional<Post> optPost = postRepository.findById(id);
@@ -80,6 +102,19 @@ public class PostManagementServiceImpl extends BasePostService implements PostMa
         if (!Objects.equals(authenticatedUserId, authorId)) {
             throw new ResourceOwnershipException();
         }
+    }
+
+    /**
+     * Batch save any entities returned by
+     * {@link PostParsingService#parse(UUID, String)} for a given newly created
+     * post.
+     * 
+     * @param entities The {@link Map} of {@link List} containing {@link PostEntity}
+     *                 related to the newly created post.
+     */
+    private void savePostEntities(Map<String, List<PostEntity>> entities) {
+        postHashtagRepository.saveAll(entities.get("hashtags"));
+        postMentionRepository.saveAll(entities.get("mentions"));
     }
 
 }
