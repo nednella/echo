828d81bc869c5a43c4fd77596683d08a376ad4c2
Ben Allenden <benallenden@protonmail.com>
2025-06-10 16:15:29 +0100

    Add PostEntitiesDTO to PostDTO object

---
+++ src/main/java/com/example/echo_api/persistence/dto/response/post/PostEntitiesDTO.java
@@ -0,0 +1,14 @@
+package com.example.echo_api.persistence.dto.response.post;
+
+import java.util.List;
+
+/**
+ * Represents a standardised response format for groups of post entities.
+ */
+// @formatter:off
+public record PostEntitiesDTO(
+    // List<?> media, // TODO: implement post images
+    List<PostEntityDTO> mentions,
+    List<PostEntityDTO> hashtags
+) {}
+// @formatter:on
