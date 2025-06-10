63c0910084e6436d4a8db01c12ff92f59539ff9e
Ben Allenden <benallenden@protonmail.com>
2025-06-10 16:13:46 +0100

    Add post_hashtag and post_mention SQL schema

---
+++ src/main/resources/sql/schema/07_schema-post-entities.sql
@@ -0,0 +1,29 @@
+CREATE TABLE
+    IF NOT EXISTS "post_hashtag" (
+        id             UUID PRIMARY KEY,
+        post_id        UUID NOT NULL,
+        start_index    INTEGER NOT NULL,
+        end_index      INTEGER NOT NULL,
+        text           TEXT NOT NULL,
+        created_at     TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        CONSTRAINT fk_post_id FOREIGN KEY (post_id) REFERENCES "post"(id) ON DELETE CASCADE
+    );
+
+CREATE INDEX
+    IF NOT EXISTS idx_post_hashtag_post_id_text
+        ON "post_hashtag"(post_id, LOWER(text));
+
+CREATE TABLE
+    IF NOT EXISTS "post_mention" (
+        id             UUID PRIMARY KEY,
+        post_id        UUID NOT NULL,
+        start_index    INTEGER NOT NULL,
+        end_index      INTEGER NOT NULL,
+        text           TEXT NOT NULL,
+        created_at     TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        CONSTRAINT fk_post_id FOREIGN KEY (post_id) REFERENCES "post"(id) ON DELETE CASCADE
+    );
+
+CREATE INDEX 
+    IF NOT EXISTS idx_post_mention_post_id_text
+        ON "post_mention"(post_id, LOWER(text));
