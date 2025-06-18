34b789b832a1dc8b7b6c726f762cb9d1b3a1c839
nednella <119449811+nednella@users.noreply.github.com>
2025-06-18 16:49:26 +0100

    fix: repository primary key references (#46)
    ## Purpose
    PR introduces a fix to the primary key references within post
    entity-related repositories
    
    ... how did gradle complete a build and test with a clear type error?!?!
    
    ## Changelog
    ### Persistence
    - Update `PostHashtagRepository` and `PostMentionRepository` primary key
    references from `UUID` to `PostEntityPK`

--- src/main/java/com/example/echo_api/persistence/repository/PostHashtagRepository.java
+++ src/main/java/com/example/echo_api/persistence/repository/PostHashtagRepository.java
@@ -1,11 +1,10 @@
 package com.example.echo_api.persistence.repository;
 
-import java.util.UUID;
-
 import org.springframework.data.repository.ListCrudRepository;
 
 import com.example.echo_api.persistence.model.post.entity.PostEntity;
+import com.example.echo_api.persistence.model.post.entity.PostEntityPK;
 
-public interface PostHashtagRepository extends ListCrudRepository<PostEntity, UUID> {
+public interface PostHashtagRepository extends ListCrudRepository<PostEntity, PostEntityPK> {
 
 }
--- src/main/java/com/example/echo_api/persistence/repository/PostMentionRepository.java
+++ src/main/java/com/example/echo_api/persistence/repository/PostMentionRepository.java
@@ -1,11 +1,10 @@
 package com.example.echo_api.persistence.repository;
 
-import java.util.UUID;
-
 import org.springframework.data.repository.ListCrudRepository;
 
 import com.example.echo_api.persistence.model.post.entity.PostEntity;
+import com.example.echo_api.persistence.model.post.entity.PostEntityPK;
 
-public interface PostMentionRepository extends ListCrudRepository<PostEntity, UUID> {
+public interface PostMentionRepository extends ListCrudRepository<PostEntity, PostEntityPK> {
 
 }
