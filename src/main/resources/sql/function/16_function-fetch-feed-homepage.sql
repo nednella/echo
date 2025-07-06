2935e41b5b14d9762bf9604a5637a83ef6d3b216
Ben Allenden <benallenden@protonmail.com>
2025-07-06 23:53:49 +0100

    Implement homepage feed SQL functions

---
+++ src/main/resources/sql/function/16_function-fetch-feed-homepage.sql
@@ -0,0 +1,59 @@
+
+/*
+ * Function fetches posts that form a users homepage.
+ *
+ * Root-level posts belonging to the user, and any users followed by the user, are fetched
+ * and sorted by creation timestamp.
+ * 
+*/
+
+CREATE OR REPLACE FUNCTION fetch_feed_homepage(
+    p_authenticated_user_id UUID,
+    p_offset INTEGER,
+    p_limit INTEGER
+)
+RETURNS TABLE (
+    id                        UUID,
+    parent_id                 UUID,
+    conversation_id           UUID,
+    text                      VARCHAR(280),
+    created_at                TIMESTAMPTZ,
+    post_like_count           BIGINT,
+    post_reply_count          BIGINT,
+    post_share_count          BIGINT,
+    post_rel_liked            BOOLEAN,
+    post_rel_shared           BOOLEAN,
+    author_is_self            BOOLEAN,
+    author_id                 UUID,
+    author_username           VARCHAR(15),
+    author_name               VARCHAR(50),
+    author_avatar_url         VARCHAR(255),
+    author_rel_following      BOOLEAN,
+    author_rel_followed_by    BOOLEAN,
+    author_rel_blocking       BOOLEAN,
+    author_rel_blocked_by     BOOLEAN,
+    post_entities             JSONB
+)
+AS
+'
+    BEGIN
+        RETURN QUERY
+        WITH homepage_posts AS (
+            SELECT
+                p.id
+            FROM post p
+            LEFT JOIN follow f ON p.author_id = f.followed_id
+            WHERE p.parent_id IS NULL
+            AND (f.follower_id = p_authenticated_user_id
+            OR p.author_id = p_authenticated_user_id)
+            ORDER BY p.created_at DESC
+            OFFSET p_offset
+            LIMIT p_limit
+        )
+        SELECT
+            fp.*
+        FROM fetch_posts(ARRAY(SELECT hp.id FROM homepage_posts hp), p_authenticated_user_id) fp
+        ORDER BY fp.created_at DESC;
+    END;
+'
+LANGUAGE plpgsql;
---
+++ src/main/resources/sql/function/17_function-fetch-feed-homepage-count.sql
@@ -0,0 +1,25 @@
+
+/*
+ * Function fetches the total count of posts that form a users homepage.
+ *
+ * To be consumed as part of a pagination implementation where the total number 
+ * of entries available are required.
+*/
+
+CREATE OR REPLACE FUNCTION fetch_feed_homepage_count(
+    p_authenticated_user_id UUID
+)
+RETURNS INTEGER
+AS
+'
+    BEGIN 
+        RETURN QUERY
+        SELECT COUNT(*)
+        FROM post p
+        LEFT JOIN follow f ON p.author_id = f.followed_id
+        WHERE p.parent_id IS NULL
+        AND (f.follower_id = p_authenticated_user_id
+            OR p.author_id = p_authenticated_user_id);
+    END;
+'
+LANGUAGE plpgsql;
