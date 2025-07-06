e69376bee760fdb4f82cf4de6d592c39724ad509
Ben Allenden <benallenden@protonmail.com>
2025-07-06 23:54:28 +0100

    Implement profile feed SQL functions

---
+++ src/main/resources/sql/function/20_function-fetch-feed-profile.sql
@@ -0,0 +1,57 @@
+
+/*
+ * Function fetches posts that form a users profile page.
+ *
+ * Root-level posts belonging to the user are fetched and sorted by creation timestamp.
+ * 
+*/
+
+CREATE OR REPLACE FUNCTION fetch_feed_profile(
+    p_profile_id UUID,
+    p_authenticated_user_id UUID,
+    p_offset INTEGER,
+    p_limit INTEGER
+)
+RETURNS TABLE (
+    id                        UUID,
+    parent_id                 UUID,
+    conversation_id           UUID,
+    text                      VARCHAR(280),
+    created_at                TIMESTAMPTZ,
+    post_like_count           BIGINT,
+    post_reply_count          BIGINT,
+    post_share_count          BIGINT,
+    post_rel_liked            BOOLEAN,
+    post_rel_shared           BOOLEAN,
+    author_is_self            BOOLEAN,
+    author_id                 UUID,
+    author_username           VARCHAR(15),
+    author_name               VARCHAR(50),
+    author_avatar_url         VARCHAR(255),
+    author_rel_following      BOOLEAN,
+    author_rel_followed_by    BOOLEAN,
+    author_rel_blocking       BOOLEAN,
+    author_rel_blocked_by     BOOLEAN,
+    post_entities             JSONB
+)
+AS
+'
+    BEGIN
+        RETURN QUERY
+        WITH profile_posts AS (
+            SELECT
+                p.id
+            FROM post p
+            WHERE p.author_id = p_profile_id
+            AND p.parent_id IS NULL
+            ORDER BY p.created_at DESC
+            OFFSET p_offset
+            LIMIT p_limit
+        )
+        SELECT
+            fp.*
+        FROM fetch_posts(ARRAY(SELECT pp.id FROM profile_posts pp), p_authenticated_user_id) fp
+        ORDER BY fp.created_at DESC;
+    END;
+'
+LANGUAGE plpgsql;
---
+++ src/main/resources/sql/function/21_function-fetch-feed-profile-count.sql
@@ -0,0 +1,23 @@
+
+/*
+ * Function fetches the total count of posts that form a users profile page.
+ *
+ * To be consumed as part of a pagination implementation where the total number 
+ * of entries available are required.
+*/
+
+CREATE OR REPLACE FUNCTION fetch_feed_profile_count(
+    p_profile_id UUID
+)
+RETURNS INTEGER
+AS
+'
+    BEGIN 
+        RETURN QUERY
+        SELECT COUNT(*)
+        FROM post p
+        WHERE p.author_id = p_profile_id
+        AND p.parent_id IS NULL;
+    END;
+'
+LANGUAGE plpgsql;
