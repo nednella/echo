0280c3e6a5e127ce3a589b8249b3ff92f182afbe
Ben Allenden <benallenden@protonmail.com>
2025-07-07 00:04:06 +0100

    Implement profile mentions feed SQL functions

---
+++ src/main/resources/sql/function/27_function-fetch-feed-profile-mentions-count.sql
@@ -0,0 +1,23 @@
+
+/*
+ * Function fetches the total count of posts that form a users profile mentions tab.
+ *
+ * To be consumed as part of a pagination implementation where the total number 
+ * of entries available are required.
+*/
+
+CREATE OR REPLACE FUNCTION fetch_feed_profile_mentions_count(
+    p_profile_id UUID
+)
+RETURNS INTEGER
+AS
+'
+    BEGIN 
+        RETURN QUERY
+        SELECT COUNT(*)
+        FROM post p
+        INNER JOIN post_entity pe ON p.id = pe.post_id AND pe.entity_type = ''MENTION''
+        INNER JOIN profile pr ON pe.text = pr.username AND pr.id = p_profile_id;
+    END;
+'
+LANGUAGE plpgsql;
