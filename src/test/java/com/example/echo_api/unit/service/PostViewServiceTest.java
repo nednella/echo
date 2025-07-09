7a50940c248b4b35cc9355505ec34a6bf4e472ef
Ben Allenden <benallenden@protonmail.com>
2025-07-09 22:25:34 +0100

    Implement PostViewService unit tests for feed methods

--- src/test/java/com/example/echo_api/unit/service/PostViewServiceTest.java
+++ src/test/java/com/example/echo_api/unit/service/PostViewServiceTest.java
@@ -27,7 +27,9 @@ import com.example.echo_api.persistence.dto.response.profile.SimplifiedProfileDT
 import com.example.echo_api.persistence.mapper.PageMapper;
 import com.example.echo_api.persistence.model.account.Account;
 import com.example.echo_api.persistence.model.post.Post;
+import com.example.echo_api.persistence.model.profile.Profile;
 import com.example.echo_api.persistence.repository.PostRepository;
+import com.example.echo_api.persistence.repository.ProfileRepository;
 import com.example.echo_api.service.post.view.PostViewService;
 import com.example.echo_api.service.post.view.PostViewServiceImpl;
 import com.example.echo_api.service.session.SessionService;
@@ -49,6 +51,9 @@ class PostViewServiceTest {
 
     @Mock
     private PostRepository postRepository;
+
+    @Mock
+    private ProfileRepository profileRepository;
 
     @Mock
     private HttpServletRequest httpServletRequest;
@@ -76,6 +81,10 @@ class PostViewServiceTest {
             new PostMetricsDTO(0, 0),
             new PostRelationshipDTO(false),
             new PostEntitiesDTO(List.of(), List.of(), List.of()));
+    }
+
+    private Profile createProfileEntity(UUID id, String username) {
+        return new Profile(id, username);
     }
 
     @Test
@@ -121,8 +130,8 @@ class PostViewServiceTest {
         Page<PostDTO> repliesDto = new PageImpl<>(List.of(createPostDto(id, "Test post.")), page, 1);
         PageDTO<PostDTO> expected = PageMapper.toDTO(repliesDto, uri);
 
-        when(sessionService.getAuthenticatedUser()).thenReturn(authenticatedUser);
         when(postRepository.findById(id)).thenReturn(Optional.of(post));
+        when(sessionService.getAuthenticatedUser()).thenReturn(authenticatedUser);
         when(postRepository.findReplyPostsById(post.getId(), authenticatedUser.getId(), page)).thenReturn(repliesDto);
         when(httpServletRequest.getRequestURI()).thenReturn(uri);
 
@@ -150,6 +159,224 @@ class PostViewServiceTest {
         assertThrows(ResourceNotFoundException.class, () -> postViewService.getPostRepliesById(id, page));
         verify(postRepository, times(1)).findById(id);
         verify(postRepository, times(0)).findReplyPostsById(any(UUID.class), eq(authenticatedUser.getId()), eq(page));
+    }
+
+    @Test
+    void PostViewService_GetHomepagePosts_ReturnPageDtoOfPostDto() {
+        // arrange
+        String uri = "/some/api/uri";
+        int offset = 0;
+        int limit = 20;
+        Pageable page = new OffsetLimitRequest(offset, limit);
+        Page<PostDTO> posts = new PageImpl<>(List.of(createPostDto(UUID.randomUUID(), "Test post.")), page, 1);
+        PageDTO<PostDTO> expected = PageMapper.toDTO(posts, uri);
+
+        when(sessionService.getAuthenticatedUser()).thenReturn(authenticatedUser);
+        when(postRepository.findHomepagePosts(authenticatedUser.getId(), page)).thenReturn(posts);
+
+        // act
+        PageDTO<PostDTO> actual = postViewService.getHomepagePosts(page);
+
+        // assert
+        assertEquals(expected, actual);
+        verify(postRepository, times(1)).findHomepagePosts(authenticatedUser.getId(), page);
+    }
+
+    @Test
+    void PostViewService_GetDiscoverPosts_ReturnPageDtoOfPostDto() {
+        // arrange
+        String uri = "/some/api/uri";
+        int offset = 0;
+        int limit = 20;
+        Pageable page = new OffsetLimitRequest(offset, limit);
+        Page<PostDTO> posts = new PageImpl<>(List.of(createPostDto(UUID.randomUUID(), "Test post.")), page, 1);
+        PageDTO<PostDTO> expected = PageMapper.toDTO(posts, uri);
+
+        when(sessionService.getAuthenticatedUser()).thenReturn(authenticatedUser);
+        when(postRepository.findDiscoverPosts(authenticatedUser.getId(), page)).thenReturn(posts);
+
+        // act
+        PageDTO<PostDTO> actual = postViewService.getDiscoverPosts(page);
+
+        // assert
+        assertEquals(expected, actual);
+        verify(postRepository, times(1)).findDiscoverPosts(authenticatedUser.getId(), page);
+    }
+
+    @Test
+    void PostViewService_GetProfilePostsByUsername_ReturnPageDtoOfPostDto() {
+        // arrange
+        String username = "random-string";
+        Profile profile = createProfileEntity(UUID.randomUUID(), username);
+
+        String uri = "/some/api/uri";
+        int offset = 0;
+        int limit = 20;
+        Pageable page = new OffsetLimitRequest(offset, limit);
+        Page<PostDTO> posts = new PageImpl<>(List.of(createPostDto(UUID.randomUUID(), "Test post.")), page, 1);
+        PageDTO<PostDTO> expected = PageMapper.toDTO(posts, uri);
+
+        when(profileRepository.findByUsername(username)).thenReturn(Optional.of(profile));
+        when(sessionService.getAuthenticatedUser()).thenReturn(authenticatedUser);
+        when(postRepository.findPostsByProfileId(profile.getId(), authenticatedUser.getId(), page)).thenReturn(posts);
+
+        // act
+        PageDTO<PostDTO> actual = postViewService.getProfilePostsByUsername(username, page);
+
+        // assert
+        assertEquals(expected, actual);
+        verify(profileRepository, times(1)).findByUsername(username);
+        verify(postRepository, times(1)).findPostsByProfileId(profile.getId(), authenticatedUser.getId(), page);
+    }
+
+    @Test
+    void PostViewService_GetProfilePostsByUsername_ThrowResourceNotFoundException() {
+        // arrange
+        String username = "random-string";
+
+        int offset = 0;
+        int limit = 20;
+        Pageable page = new OffsetLimitRequest(offset, limit);
+
+        when(profileRepository.findByUsername(username)).thenReturn(Optional.empty());
+
+        // act & assert
+        assertThrows(ResourceNotFoundException.class, () -> postViewService.getProfilePostsByUsername(username, page));
+        verify(profileRepository, times(1)).findByUsername(username);
+        verify(postRepository, times(0)).findPostsByProfileId(any(UUID.class), eq(authenticatedUser.getId()), eq(page));
+    }
+
+    @Test
+    void PostViewService_GetProfileRepliesById_ReturnPageDtoOfPostDto() {
+        // arrange
+        UUID profileId = UUID.randomUUID();
+        Profile profile = createProfileEntity(profileId, "random-string");
+
+        String uri = "/some/api/uri";
+        int offset = 0;
+        int limit = 20;
+        Pageable page = new OffsetLimitRequest(offset, limit);
+        Page<PostDTO> posts = new PageImpl<>(List.of(createPostDto(UUID.randomUUID(), "Test post.")), page, 1);
+        PageDTO<PostDTO> expected = PageMapper.toDTO(posts, uri);
+
+        when(profileRepository.findById(profileId)).thenReturn(Optional.of(profile));
+        when(sessionService.getAuthenticatedUser()).thenReturn(authenticatedUser);
+        when(postRepository.findReplyPostsByProfileId(profileId, authenticatedUser.getId(), page)).thenReturn(posts);
+
+        // act
+        PageDTO<PostDTO> actual = postViewService.getProfileRepliesById(profileId, page);
+
+        // assert
+        assertEquals(expected, actual);
+        verify(profileRepository, times(1)).findById(profileId);
+        verify(postRepository, times(1)).findReplyPostsByProfileId(profile.getId(), authenticatedUser.getId(), page);
+    }
+
+    @Test
+    void PostViewService_GetProfileRepliesById_ThrowResourceNotFoundException() {
+        // arrange
+        UUID profileId = UUID.randomUUID();
+
+        int offset = 0;
+        int limit = 20;
+        Pageable page = new OffsetLimitRequest(offset, limit);
+
+        when(profileRepository.findById(profileId)).thenReturn(Optional.empty());
+
+        // act & assert
+        assertThrows(ResourceNotFoundException.class, () -> postViewService.getProfileRepliesById(profileId, page));
+        verify(profileRepository, times(1)).findById(profileId);
+        verify(postRepository, times(0)).findReplyPostsByProfileId(any(UUID.class), eq(authenticatedUser.getId()),
+            eq(page));
+    }
+
+    @Test
+    void PostViewService_GetProfileLikesById_ReturnPageDtoOfPostDto() {
+        // arrange
+        UUID profileId = UUID.randomUUID();
+        Profile profile = createProfileEntity(profileId, "random-string");
+
+        String uri = "/some/api/uri";
+        int offset = 0;
+        int limit = 20;
+        Pageable page = new OffsetLimitRequest(offset, limit);
+        Page<PostDTO> posts = new PageImpl<>(List.of(createPostDto(UUID.randomUUID(), "Test post.")), page, 1);
+        PageDTO<PostDTO> expected = PageMapper.toDTO(posts, uri);
+
+        when(profileRepository.findById(profileId)).thenReturn(Optional.of(profile));
+        when(sessionService.getAuthenticatedUser()).thenReturn(authenticatedUser);
+        when(postRepository.findLikedPostsByProfileId(profileId, authenticatedUser.getId(), page)).thenReturn(posts);
+
+        // act
+        PageDTO<PostDTO> actual = postViewService.getProfileLikesById(profileId, page);
+
+        // assert
+        assertEquals(expected, actual);
+        verify(profileRepository, times(1)).findById(profileId);
+        verify(postRepository, times(1)).findLikedPostsByProfileId(profile.getId(), authenticatedUser.getId(), page);
+    }
+
+    @Test
+    void PostViewService_GetProfileLikesById_ThrowResourceNotFoundException() {
+        // arrange
+        UUID profileId = UUID.randomUUID();
+
+        int offset = 0;
+        int limit = 20;
+        Pageable page = new OffsetLimitRequest(offset, limit);
+
+        when(profileRepository.findById(profileId)).thenReturn(Optional.empty());
+
+        // act & assert
+        assertThrows(ResourceNotFoundException.class, () -> postViewService.getProfileLikesById(profileId, page));
+        verify(profileRepository, times(1)).findById(profileId);
+        verify(postRepository, times(0)).findReplyPostsById(any(UUID.class), eq(authenticatedUser.getId()), eq(page));
+    }
+
+    @Test
+    void PostViewService_GetProfileMentionsById_ReturnPageDtoOfPostDto() {
+        // arrange
+        UUID profileId = UUID.randomUUID();
+        Profile profile = createProfileEntity(profileId, "random-string");
+
+        String uri = "/some/api/uri";
+        int offset = 0;
+        int limit = 20;
+        Pageable page = new OffsetLimitRequest(offset, limit);
+        Page<PostDTO> posts = new PageImpl<>(List.of(createPostDto(UUID.randomUUID(), "Test post.")), page, 1);
+        PageDTO<PostDTO> expected = PageMapper.toDTO(posts, uri);
+
+        when(profileRepository.findById(profileId)).thenReturn(Optional.of(profile));
+        when(sessionService.getAuthenticatedUser()).thenReturn(authenticatedUser);
+        when(postRepository.findMentionedPostsByProfileId(profileId, authenticatedUser.getId(), page))
+            .thenReturn(posts);
+
+        // act
+        PageDTO<PostDTO> actual = postViewService.getProfileMentionsById(profileId, page);
+
+        // assert
+        assertEquals(expected, actual);
+        verify(profileRepository, times(1)).findById(profileId);
+        verify(postRepository, times(1)).findMentionedPostsByProfileId(profile.getId(), authenticatedUser.getId(),
+            page);
+    }
+
+    @Test
+    void PostViewService_GetProfileMentionsById_ThrowResourceNotFoundException() {
+        // arrange
+        UUID profileId = UUID.randomUUID();
+
+        int offset = 0;
+        int limit = 20;
+        Pageable page = new OffsetLimitRequest(offset, limit);
+
+        when(profileRepository.findById(profileId)).thenReturn(Optional.empty());
+
+        // act & assert
+        assertThrows(ResourceNotFoundException.class, () -> postViewService.getProfileMentionsById(profileId, page));
+        verify(profileRepository, times(1)).findById(profileId);
+        verify(postRepository, times(0)).findMentionedPostsByProfileId(any(UUID.class), eq(authenticatedUser.getId()),
+            eq(page));
     }
 
 }
