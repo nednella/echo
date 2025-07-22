b2d93978e65ba8f9af38d591a92172ea5ee658d5
Ben Allenden <benallenden@protonmail.com>
2025-07-22 18:42:24 +0100

    Update prettier config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
+import { useState } from "react"
 
 export default function App() {
     const [count, setCount] = useState(0)
 
     return (
-        <div className='flex h-full flex-col items-center justify-center gap-6'>
+        <div className="flex h-full flex-col items-center justify-center gap-6">
             <h1>Echo</h1>
             <button
-                className='pl cursor-pointer rounded-lg border border-white px-4 py-2 transition-colors
-                    hover:border-[#646cff]'
+                className="pl cursor-pointer rounded-lg border border-white px-4 py-2 transition-colors
+                    hover:border-[#646cff]"
                 onClick={() => setCount((count) => count + 1)}
             >
                 count is {count}
