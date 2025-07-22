32019db1c32a5985512eda53ef352e7e67ab92e1
Ben Allenden <benallenden@protonmail.com>
2025-07-22 22:33:00 +0100

    Move application entry point to its own directory

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -5,7 +5,7 @@ export default function App() {
 
     return (
         <div className="flex h-full flex-col items-center justify-center gap-6">
-            <h1>Echo</h1>
+            <h1 className="text-3xl font-medium">Echo</h1>
             <button
                 className="pl cursor-pointer rounded-lg border border-white px-4 py-2 transition-colors
                     hover:border-[#646cff]"
