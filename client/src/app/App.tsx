ec65c15ee6e179ed1abec0449153f8411e6ac38f
Ben Allenden <benallenden@protonmail.com>
2025-07-26 20:34:28 +0100

    Setup TanStack Router w/ placeholder public pages

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,18 +1,14 @@
-import { useState } from "react"
+import { routeTree } from "../routeTree.gen"
+import { RouterProvider, createRouter } from "@tanstack/react-router"
+
+const router = createRouter({ routeTree })
+
+declare module "@tanstack/react-router" {
+    interface Register {
+        router: typeof router
+    }
+}
 
 export default function App() {
-    const [count, setCount] = useState(0)
-
-    return (
-        <div className="flex h-full flex-col items-center justify-center gap-6">
-            <h1 className="text-3xl font-medium">Echo</h1>
-            <button
-                className="pl cursor-pointer rounded-lg border border-white px-4 py-2 transition-colors
-                    hover:border-[#646cff]"
-                onClick={() => setCount((count) => count + 1)}
-            >
-                count is {count}
-            </button>
-        </div>
-    )
+    return <RouterProvider router={router} />
 }
---
+++ client/src/routes/_public/index.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+import { createFileRoute } from "@tanstack/react-router"
+
+export const Route = createFileRoute("/_public/")({
+    component: Index
+})
+
+function Index() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <div className="flex h-full flex-col items-center justify-center gap-6">
+            <h1 className="text-3xl font-medium">Echo</h1>
+            <button
+                className="pl cursor-pointer rounded-lg border border-white px-4 py-2 transition-colors
+                    hover:border-[#646cff]"
+                onClick={() => setCount((count) => count + 1)}
+            >
+                count is {count}
+            </button>
+        </div>
+    )
+}
