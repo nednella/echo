a64a8dafff57eb6900b1822db6369874596f88bf
Ben Allenden <benallenden@protonmail.com>
2025-07-31 19:11:19 +0100

    Add getAuthenticatedUser id/clerkId methods to SessionService

--- api/src/main/java/com/example/echo_api/service/session/SessionService.java
+++ api/src/main/java/com/example/echo_api/service/session/SessionService.java
@@ -1,10 +1,13 @@
 package com.example.echo_api.service.session;
 
+import java.util.UUID;
+
+import com.example.echo_api.exception.custom.badrequest.OnboardingIncompleteException;
+
 public interface SessionService {
 
-    /**
-     * 
-     */
-    public void getAuthenticatedUser();
+    public UUID getAuthenticatedUserId() throws OnboardingIncompleteException;
+
+    public String getAuthenticatedUserClerkId();
 
 }
--- api/src/main/java/com/example/echo_api/service/session/SessionServiceImpl.java
+++ api/src/main/java/com/example/echo_api/service/session/SessionServiceImpl.java
@@ -1,19 +1,44 @@
 package com.example.echo_api.service.session;
 
+import java.util.UUID;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+
+import com.example.echo_api.exception.custom.badrequest.OnboardingIncompleteException;
+import com.example.echo_api.persistence.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
-/**
- * 
- */
 @Service
 @RequiredArgsConstructor
 public class SessionServiceImpl implements SessionService {
 
+    private final UserRepository userRepository;
+
     @Override
-    public void getAuthenticatedUser() {
-        throw new UnsupportedOperationException();
+    public UUID getAuthenticatedUserId() throws OnboardingIncompleteException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Jwt token = (Jwt) authentication.getPrincipal();
+        String userId = token.getClaim("echo_id");
+
+        try {
+            return UUID.fromString(userId);
+        } catch (Exception ex) {
+            return userRepository.findIdByClerkId(token.getSubject())
+                    .orElseThrow(OnboardingIncompleteException::new);
+        }
+    }
+
+    @Override
+    public String getAuthenticatedUserClerkId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Jwt token = (Jwt) authentication.getPrincipal();
+
+        return token.getSubject();
     }
 
 }
