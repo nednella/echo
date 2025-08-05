0fe517589a644c29ef0d9ffe29009891135f60a1
Ben Allenden <benallenden@protonmail.com>
2025-08-05 20:29:16 +0100

    Add webhookService deserializePayload method

--- api/src/main/java/com/example/echo_api/service/webhook/WebhookService.java
+++ api/src/main/java/com/example/echo_api/service/webhook/WebhookService.java
@@ -1,5 +1,19 @@
 package com.example.echo_api.service.webhook;
 
+import com.example.echo_api.exception.custom.badrequest.DeserializationException;
+import com.example.echo_api.persistence.dto.request.webhook.WebhookEvent;
+
 public interface WebhookService {
+
+    /**
+     * Deserializes a JSON payload string into a {@link WebhookEvent} referencing
+     * the type of event being sent from Clerk via its {@code type} field.
+     * 
+     * @param payload The JSON payload string to deserialize.
+     * @return The mapped {@link WebhookEvent}.
+     * @throws DeserializationException If there was an issue when deserializing the
+     *                                  JSON payload for whatever reason.
+     */
+    public WebhookEvent deserializePayload(String payload);
 
 }
--- api/src/main/java/com/example/echo_api/service/webhook/WebhookServiceImpl.java
+++ api/src/main/java/com/example/echo_api/service/webhook/WebhookServiceImpl.java
@@ -1,5 +1,31 @@
 package com.example.echo_api.service.webhook;
 
+import org.springframework.stereotype.Service;
+
+import com.clerk.backend_api.Clerk;
+import com.example.echo_api.exception.custom.badrequest.DeserializationException;
+import com.example.echo_api.persistence.dto.request.webhook.WebhookEvent;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Service implementation for acting upon received asynchronous {@link Clerk}
+ * webhook notifications to ensure database synchronisation.
+ */
+@Service
+@RequiredArgsConstructor
 public class WebhookServiceImpl implements WebhookService {
+
+    private final ObjectMapper mapper;
+
+    @Override
+    public WebhookEvent deserializePayload(String payload) {
+        try {
+            return mapper.readValue(payload, WebhookEvent.class);
+        } catch (Exception ex) {
+            throw new DeserializationException(ex.getMessage());
+        }
+    }
 
 }
