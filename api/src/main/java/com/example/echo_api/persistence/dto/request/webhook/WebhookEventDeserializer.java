cac5dff9f519c501769a04c2ea37c258a6d36b94
Ben Allenden <benallenden@protonmail.com>
2025-08-05 20:27:34 +0100

    Add custom DeserializationException

--- api/src/main/java/com/example/echo_api/persistence/dto/request/webhook/WebhookEventDeserializer.java
+++ api/src/main/java/com/example/echo_api/persistence/dto/request/webhook/WebhookEventDeserializer.java
@@ -1,7 +1,6 @@
 package com.example.echo_api.persistence.dto.request.webhook;
 
-import java.io.IOException;
-
+import com.example.echo_api.exception.custom.badrequest.DeserializationException;
 import com.example.echo_api.persistence.dto.request.webhook.data.UserCreated;
 import com.example.echo_api.persistence.dto.request.webhook.data.UserDeleted;
 import com.example.echo_api.persistence.dto.request.webhook.data.UserUpdated;
@@ -16,23 +15,24 @@ public class WebhookEventDeserializer extends JsonDeserializer<WebhookEvent> {
     private static final String TYPE_FIELD = "type";
 
     @Override
-    public WebhookEvent deserialize(JsonParser p, DeserializationContext ctx)
-        throws IOException, IllegalArgumentException {
-        JsonNode node = p.readValueAsTree();
+    public WebhookEvent deserialize(JsonParser p, DeserializationContext ctx) throws DeserializationException {
+        try {
+            JsonNode node = p.readValueAsTree();
+            JsonNode dataNode = node.get(DATA_FIELD);
+            JsonNode typeNode = node.get(TYPE_FIELD);
+            if (dataNode == null) {
+                throw new IllegalArgumentException("Missing \"" + DATA_FIELD + "\" field in JSON payload");
+            }
+            if (typeNode == null) {
+                throw new IllegalArgumentException("Missing \"" + TYPE_FIELD + "\" field in JSON payload");
+            }
 
-        JsonNode dataNode = node.get(DATA_FIELD);
-        JsonNode typeNode = node.get(TYPE_FIELD);
-        if (dataNode == null) {
-            throw new IllegalArgumentException("Missing \"" + DATA_FIELD + "\" field in JSON payload");
+            WebhookEventType type = ctx.readTreeAsValue(typeNode, WebhookEventType.class);
+            WebhookEventData data = ctx.readTreeAsValue(dataNode, resolveWebhookEventDataType(type));
+            return new WebhookEvent(data, type);
+        } catch (Exception ex) {
+            throw new DeserializationException(ex.getMessage());
         }
-        if (typeNode == null) {
-            throw new IllegalArgumentException("Missing \"" + TYPE_FIELD + "\" field in JSON payload");
-        }
-
-        WebhookEventType type = ctx.readTreeAsValue(typeNode, WebhookEventType.class);
-        WebhookEventData data = ctx.readTreeAsValue(dataNode, resolveWebhookEventDataType(type));
-
-        return new WebhookEvent(data, type);
     }
 
     private Class<? extends WebhookEventData> resolveWebhookEventDataType(WebhookEventType type)
