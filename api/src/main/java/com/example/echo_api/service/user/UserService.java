d01b6c3a9a32edf47f0ad744fe48be3db3bbdcda
Ben Allenden <benallenden@protonmail.com>
2025-08-07 04:02:39 +0100

    Update UserService Jdoc

--- api/src/main/java/com/example/echo_api/service/user/UserService.java
+++ api/src/main/java/com/example/echo_api/service/user/UserService.java
@@ -6,16 +6,16 @@ import com.example.echo_api.persistence.model.user.User;
 public interface UserService {
 
     /**
-     * Create a new {@link User} with an associated {@link Profile} in a single
-     * operation.
+     * Create a new {@link User} with an associated {@link Profile} as part of the
+     * Clerk database synchronisation process.
      * 
      * @param clerkId  The unique identifier from Clerk
      * @param username The unique username from Clerk
      * @param imageUrl The optional profile image URL from Clerk
      * @return The newly created {@link User} entity
-     * @throws ClerkIdAlreadyExistsException If a {@link User} with the supplied
-     *                                       {@code clerkId} already exists in the
-     *                                       local system
+     * @throws ClerkIdAlreadyExistsException If a {@link User} entity with the
+     *                                       supplied {@code clerkId} already exists
+     *                                       in the local database
      */
     public User createUserWithProfile(String clerkId, String username, String imageUrl);
 
