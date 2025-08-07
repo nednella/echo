1f79af4937dec4f6bb0f914952151b90a74e5a22
Ben Allenden <benallenden@protonmail.com>
2025-08-07 22:46:04 +0100

    Add UserRepository methods findByClerkId, deleteByClerkId

--- api/src/main/java/com/example/echo_api/persistence/repository/UserRepository.java
+++ api/src/main/java/com/example/echo_api/persistence/repository/UserRepository.java
@@ -13,11 +13,20 @@ import com.example.echo_api.persistence.model.user.User;
 public interface UserRepository extends ListCrudRepository<User, UUID> {
 
     /**
+     * Find a {@link User} by {@code clerkId}.
+     * 
+     * @param clerkId The {@code clerkId} to search for
+     * @return An {@link Optional} containing the {@link User} if found, otherwise
+     *         empty
+     */
+    Optional<User> findByClerkId(String clerkId);
+
+    /**
      * Find a user {@link UUID} by {@code clerkId}.
      * 
-     * @param clerkId The {@code clerkId} to search for.
+     * @param clerkId The {@code clerkId} to search for
      * @return An {@link Optional} containing the user's {@link UUID} if found,
-     *         otherwise empty.
+     *         otherwise empty
      */
     @Query("SELECT u.id FROM User u WHERE u.clerkId = :clerkId")
     Optional<UUID> findIdByClerkId(String clerkId);
@@ -25,9 +34,17 @@ public interface UserRepository extends ListCrudRepository<User, UUID> {
     /**
      * Check if a {@link User} exists by {@code clerkId}.
      * 
-     * @param clerkId The {@code clerkId} to search for.
-     * @return True if exists, else false.
+     * @param clerkId The {@code clerkId} to search for
+     * @return True if exists, else false
      */
     boolean existsByClerkId(String clerkId);
+
+    /**
+     * Deletes the {@link User} with the given {@code clerkId}.
+     * 
+     * @param clerkId The clerk id of the user
+     * @return The number of records deleted (0 or 1)
+     */
+    int deleteByClerkId(String clerkId);
 
 }
