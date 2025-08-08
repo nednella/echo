6603608cc94e334c855007c7b4001b31e95a9ee5
Ben Allenden <benallenden@protonmail.com>
2025-08-08 01:05:11 +0100

    Add UserService handlers for Clerk user update/delete notifications received via webhook

--- api/src/main/java/com/example/echo_api/service/user/UserService.java
+++ api/src/main/java/com/example/echo_api/service/user/UserService.java
@@ -1,13 +1,16 @@
 package com.example.echo_api.service.user;
 
 import com.example.echo_api.exception.custom.badrequest.ClerkIdAlreadyExistsException;
+import com.example.echo_api.exception.custom.notfound.ResourceNotFoundException;
+import com.example.echo_api.persistence.dto.request.webhook.clerk.data.UserDeleted;
+import com.example.echo_api.persistence.dto.request.webhook.clerk.data.UserUpdated;
 import com.example.echo_api.persistence.model.user.User;
 
 public interface UserService {
 
     /**
      * Create a new {@link User} with an associated {@link Profile} as part of the
-     * Clerk database synchronisation process.
+     * Clerk user onboarding process.
      * 
      * @param clerkId  The unique identifier from Clerk
      * @param username The unique username from Clerk
@@ -18,5 +21,25 @@ public interface UserService {
      *                                       in the local database
      */
     public User createUserWithProfile(String clerkId, String username, String imageUrl);
+
+    /**
+     * Handle updates to user data originating from a Clerk webhook for local
+     * database synchronisation.
+     * 
+     * @param data The {@link UserUpdated} event payload containing the relevant
+     *             user information to update
+     * @throws ResourceNotFoundException If no {@link User} matching the payload
+     *                                   Clerk ID exists
+     */
+    public void handleClerkUserUpdated(UserUpdated data);
+
+    /**
+     * Handle user deletions originating from a Clerk webhook for local database
+     * synchronisation.
+     * 
+     * @param data The {@link UserDeleted} event payload containing the Clerk ID of
+     *             the deleted user
+     */
+    public void handleClerkUserDeleted(UserDeleted data);
 
 }
--- api/src/main/java/com/example/echo_api/service/user/UserServiceImpl.java
+++ api/src/main/java/com/example/echo_api/service/user/UserServiceImpl.java
@@ -1,9 +1,14 @@
 package com.example.echo_api.service.user;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.echo_api.exception.custom.badrequest.ClerkIdAlreadyExistsException;
+import com.example.echo_api.exception.custom.notfound.ResourceNotFoundException;
+import com.example.echo_api.persistence.dto.request.webhook.clerk.data.UserDeleted;
+import com.example.echo_api.persistence.dto.request.webhook.clerk.data.UserUpdated;
 import com.example.echo_api.persistence.model.profile.Profile;
 import com.example.echo_api.persistence.model.user.User;
 import com.example.echo_api.persistence.repository.ProfileRepository;
@@ -15,6 +20,7 @@ import lombok.RequiredArgsConstructor;
  * Service implementation for managing CRUD operations of {@link User} entities.
  */
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
@@ -35,6 +41,47 @@ public class UserServiceImpl implements UserService {
         profileRepository.save(profile);
 
         return user;
+    }
+
+    @Override
+    public void handleClerkUserUpdated(UserUpdated data) {
+        User user = getUserEntityByClerkId(data.id());
+        Profile profile = getProfileEntityById(user.getId());
+
+        user.setUsername(data.username());
+        profile.setAvatarImageUrl(data.imageUrl());
+
+        userRepository.save(user);
+        profileRepository.save(profile);
+    }
+
+    @Override
+    public void handleClerkUserDeleted(UserDeleted data) {
+        userRepository.deleteByClerkId(data.id());
+    }
+
+    /**
+     * Retrieve a {@link User} entity by its associated Clerk ID.
+     * 
+     * @param clerkId the Clerk ID of the user
+     * @return The {@link User} entity
+     * @throws ResourceNotFoundException If no user by that Clerk ID exists
+     */
+    private User getUserEntityByClerkId(String clerkId) throws ResourceNotFoundException {
+        return userRepository.findByClerkId(clerkId)
+            .orElseThrow(ResourceNotFoundException::new);
+    }
+
+    /**
+     * Retrieve a {@link Profile} via its {@link UUID}.
+     * 
+     * @param id The UUID of the profile
+     * @return The {@link Profile} entity
+     * @throws ResourceNotFoundException If no profile by that id exists
+     */
+    private Profile getProfileEntityById(UUID id) throws ResourceNotFoundException {
+        return profileRepository.findById(id)
+            .orElseThrow(ResourceNotFoundException::new);
     }
 
 }
