fa9bbc70b8496fe1e96b7872fa46edc8506e603c
Ben Allenden <benallenden@protonmail.com>
2025-08-28 00:41:44 +0100

    Fix unit/integration tests

--- api/src/test/java/com/example/echo_api/integration/controller/profile/ProfileManagementControllerIT.java
+++ api/src/test/java/com/example/echo_api/integration/controller/profile/ProfileManagementControllerIT.java
@@ -13,7 +13,7 @@ import com.example.echo_api.config.ErrorMessageConfig;
 import com.example.echo_api.config.ValidationMessageConfig;
 import com.example.echo_api.controller.profile.ProfileManagementController;
 import com.example.echo_api.integration.util.IntegrationTest;
-import com.example.echo_api.persistence.dto.request.profile.UpdateInformationDTO;
+import com.example.echo_api.persistence.dto.request.profile.UpdateProfileDTO;
 import com.example.echo_api.persistence.dto.response.error.ErrorDTO;
 
 /**
@@ -21,15 +21,15 @@ import com.example.echo_api.persistence.dto.response.error.ErrorDTO;
  */
 class ProfileManagementControllerIT extends IntegrationTest {
 
-    private static final String ME_INFO_PATH = ApiConfig.Profile.ME_INFO;
+    private static final String ME_PATH = ApiConfig.Profile.ME;
 
     @Test
-    void updateInformation_Returns204NoContent_WhenPassesValidation() {
+    void updateProfile_Returns204NoContent_WhenPassesValidation() {
         // api: PUT /api/v1/profile/me/info ==> 204 No Content
-        var body = new UpdateInformationDTO("x".repeat(50), "x".repeat(160), "x".repeat(30));
+        var body = new UpdateProfileDTO("x".repeat(50), "x".repeat(160), "x".repeat(30));
 
         authenticatedClient.put()
-            .uri(ME_INFO_PATH)
+            .uri(ME_PATH)
             .bodyValue(body)
             .exchange()
             .expectStatus().isNoContent()
@@ -40,11 +40,11 @@ class ProfileManagementControllerIT extends IntegrationTest {
         return Stream.of(Arguments.of("x".repeat(51), ValidationMessageConfig.NAME_TOO_LONG));
     }
 
-    @ParameterizedTest(name = "updateInformation_Returns400BadRequest_WhenProfileNameFieldIsInvalid: \"{0}\"")
+    @ParameterizedTest(name = "updateProfile_Returns400BadRequest_WhenProfileNameFieldIsInvalid: \"{0}\"")
     @MethodSource("invalidNameCases")
-    void updateInformation_Returns400BadRequest_WhenProfileNameFieldIsInvalid(String name, String expectedDetails) {
+    void updateProfile_Returns400BadRequest_WhenProfileNameFieldIsInvalid(String name, String expectedDetails) {
         // api: PUT /api/v1/profile/me/info ==> 204 No Content
-        var body = new UpdateInformationDTO(name, "x", "location");
+        var body = new UpdateProfileDTO(name, "x", "location");
 
         ErrorDTO expected = new ErrorDTO(
             HttpStatus.BAD_REQUEST,
@@ -53,7 +53,7 @@ class ProfileManagementControllerIT extends IntegrationTest {
             null);
 
         authenticatedClient.put()
-            .uri(ME_INFO_PATH)
+            .uri(ME_PATH)
             .bodyValue(body)
             .exchange()
             .expectStatus().isBadRequest()
@@ -64,11 +64,11 @@ class ProfileManagementControllerIT extends IntegrationTest {
         return Stream.of(Arguments.of("x".repeat(161), ValidationMessageConfig.BIO_TOO_LONG));
     }
 
-    @ParameterizedTest(name = "updateInformation_Returns400BadRequest_WhenProfileBioFieldIsInvalid: \"{0}\"")
+    @ParameterizedTest(name = "updateProfile_Returns400BadRequest_WhenProfileBioFieldIsInvalid: \"{0}\"")
     @MethodSource("invalidBioCases")
-    void updateInformation_Returns400BadRequest_WhenProfileBioFieldIsInvalid(String bio, String expectedDetails) {
+    void updateProfile_Returns400BadRequest_WhenProfileBioFieldIsInvalid(String bio, String expectedDetails) {
         // api: PUT /api/v1/profile/me/info ==> 204 No Content
-        var body = new UpdateInformationDTO("name", bio, "location");
+        var body = new UpdateProfileDTO("name", bio, "location");
 
         ErrorDTO expected = new ErrorDTO(
             HttpStatus.BAD_REQUEST,
@@ -77,7 +77,7 @@ class ProfileManagementControllerIT extends IntegrationTest {
             null);
 
         authenticatedClient.put()
-            .uri(ME_INFO_PATH)
+            .uri(ME_PATH)
             .bodyValue(body)
             .exchange()
             .expectStatus().isBadRequest()
@@ -88,11 +88,11 @@ class ProfileManagementControllerIT extends IntegrationTest {
         return Stream.of(Arguments.of("x".repeat(31), ValidationMessageConfig.LOCATION_TOO_LONG));
     }
 
-    @ParameterizedTest(name = "updateInformation_Returns400BadRequest_WhenProfileLocationFieldIsInvalid: \"{0}\"")
+    @ParameterizedTest(name = "updateProfile_Returns400BadRequest_WhenProfileLocationFieldIsInvalid: \"{0}\"")
     @MethodSource("invalidLocationCases")
-    void updateInformation_Returns400BadRequest_WhenProfileLocationFieldIsInvalid(String loc, String expectedDetails) {
+    void updateProfile_Returns400BadRequest_WhenProfileLocationFieldIsInvalid(String loc, String expectedDetails) {
         // api: PUT /api/v1/profile/me/info ==> 204 No Content
-        var body = new UpdateInformationDTO("name", "bio", loc);
+        var body = new UpdateProfileDTO("name", "bio", loc);
 
         ErrorDTO expected = new ErrorDTO(
             HttpStatus.BAD_REQUEST,
@@ -101,7 +101,7 @@ class ProfileManagementControllerIT extends IntegrationTest {
             null);
 
         authenticatedClient.put()
-            .uri(ME_INFO_PATH)
+            .uri(ME_PATH)
             .bodyValue(body)
             .exchange()
             .expectStatus().isBadRequest()
