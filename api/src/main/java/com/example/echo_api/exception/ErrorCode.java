6dcd880f700a2053012e8735c2b3037694195fe6
Ben Allenden <benallenden@protonmail.com>
2025-09-03 19:24:40 +0100

    Add default buildAsException method to ErrorCode interface for generating ApplicationException using a simple API

--- api/src/main/java/com/example/echo_api/exception/ErrorCode.java
+++ api/src/main/java/com/example/echo_api/exception/ErrorCode.java
@@ -3,8 +3,8 @@ package com.example.echo_api.exception;
 import org.springframework.http.HttpStatus;
 
 /**
- * Defines a contract for error codes that are used to build uniform API error
- * responses.
+ * Defines a contract for {@link ErrorCode}s that are used to build uniform API
+ * error responses.
  * 
  * <p>
  * Should be implemented by {@code enum} classes, where each implementing class
@@ -20,14 +20,14 @@ import org.springframework.http.HttpStatus;
 public interface ErrorCode {
 
     /**
-     * Returns the HTTP status associated with the error.
+     * Returns the HTTP status associated with this {@link ErrorCode}.
      * 
      * @return {@link HttpStatus} that should be returned to the client
      */
     HttpStatus getStatus();
 
     /**
-     * Returns the error message template for the error.
+     * Returns the error message template for this {@link ErrorCode}.
      * 
      * <p>
      * This template is typically a {@link String#format}-compatible pattern, for
@@ -38,37 +38,59 @@ public interface ErrorCode {
     String getMessageTemplate();
 
     /**
-     * Returns the number of arguments expected by the message template.
+     * Returns the number of arguments expected by this {@link ErrorCode} message
+     * template.
      * 
      * @return the number of arguments the template expects
      */
     int getExpectedArgs();
 
     /**
-     * Formats the message template with the provided arguments.
-     * 
-     * <p>
-     * This method verifies that the number of supplied arguments matches the
-     * {@link #getExpectedArgs() expected} count. If there is a mismatch, an
-     * {@link IllegalArgumentException} is thrown.
+     * Formats this {@link ErrorCode} message template with the provided arguments.
      * 
      * @param args the arguments to insert into the message template
-     * @return the formatted error message
+     * @return the formatted error message to be returned to the client
      * @throws IllegalArgumentException if the number of provided arguments does not
-     *                                  match {@link #getExpectedArgs()}
+     *                                  match the expected for this
+     *                                  {@link ErrorCode}
      */
     default String formatMessage(Object... args) {
-        int actual = args.length;
-        int expected = getExpectedArgs();
+        validateArgumentCount(args.length);
+        return String.format(getMessageTemplate(), args);
+    }
 
+    /**
+     * Return an {@link ApplicationException} representing this {@link ErrorCode}
+     * with the provided arguments.
+     * 
+     * @param args the arguments to insert into the message template
+     * @return the {@link ApplicationException} to be thrown
+     * @throws IllegalArgumentException if the number of provided arguments does not
+     *                                  match the expected for this
+     *                                  {@link ErrorCode}
+     */
+    default ApplicationException buildAsException(Object... args) {
+        validateArgumentCount(args.length);
+        return new ApplicationException(this, args);
+    }
+
+    /**
+     * validate the number of arguments supplied against the expected count for this
+     * {@link ErrorCode}.
+     * 
+     * @param actual the actual number of arguments provided
+     * @throws IllegalArgumentException if the number of provided arguments does not
+     *                                  match the expected for this
+     *                                  {@link ErrorCode}
+     */
+    private void validateArgumentCount(int actual) {
+        int expected = getExpectedArgs();
         if (actual != expected) {
             String name = getClass().getSimpleName();
 
             throw new IllegalArgumentException(
                 String.format("Expected %d arguments for %s, but got %d", expected, name, actual));
         }
-
-        return String.format(getMessageTemplate(), args);
     }
 
 }
