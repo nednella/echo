cd3346d44e6af9b8cb189474fb11de6954d3eecf
Ben Allenden <benallenden@protonmail.com>
2025-09-06 00:28:11 +0100

    Fix some tests

--- api/src/test/java/com/example/echo_api/modules/profile/service/ProfileInteractionServiceTest.java
+++ api/src/test/java/com/example/echo_api/modules/profile/service/ProfileInteractionServiceTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-import java.util.Optional;
 import java.util.UUID;
 
 import org.junit.jupiter.api.BeforeAll;
@@ -17,7 +16,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.example.echo_api.exception.ApplicationException;
 import com.example.echo_api.modules.profile.entity.ProfileFollow;
-import com.example.echo_api.modules.profile.entity.Profile;
 import com.example.echo_api.modules.profile.exception.ProfileErrorCode;
 import com.example.echo_api.modules.profile.repository.ProfileFollowRepository;
 import com.example.echo_api.modules.profile.repository.ProfileRepository;
@@ -42,23 +40,20 @@ class ProfileInteractionServiceTest {
     private ProfileFollowRepository profileFollowRepository;
 
     private static UUID authenticatedUserId;
-    private static Profile authenticatedUserProfile;
-    private static Profile target;
 
     @BeforeAll
     static void setup() {
         authenticatedUserId = UUID.randomUUID();
-        authenticatedUserProfile = Profile.forTest(authenticatedUserId, "test");
-        target = Profile.forTest(UUID.randomUUID(), "target");
     }
 
     @Test
     void follow_ReturnsVoid_WhenFollowSuccessfullyCreated() {
         // arrange
-        UUID id = target.getId();
+        UUID id = UUID.randomUUID();
 
+        when(profileRepository.existsById(id)).thenReturn(true);
         when(sessionService.getAuthenticatedUserId()).thenReturn(authenticatedUserId);
-        when(profileRepository.findById(id)).thenReturn(Optional.of(target));
+        when(profileFollowRepository.existsByFollowerIdAndFollowedId(authenticatedUserId, id)).thenReturn(false);
 
         // act & assert
         assertDoesNotThrow(() -> profileInteractionService.follow(id));
@@ -71,8 +66,7 @@ class ProfileInteractionServiceTest {
         ProfileErrorCode errorCode = ProfileErrorCode.ID_NOT_FOUND;
         UUID id = UUID.randomUUID();
 
-        when(sessionService.getAuthenticatedUserId()).thenReturn(authenticatedUserId);
-        when(profileRepository.findById(id)).thenReturn(Optional.empty());
+        when(profileRepository.existsById(id)).thenReturn(false);
 
         // act & assert
         var ex = assertThrows(ApplicationException.class, () -> profileInteractionService.follow(id));
@@ -85,13 +79,12 @@ class ProfileInteractionServiceTest {
     void follow_ThrowsApplicationException_WhenProfileByIdIsYou() {
         // arrange
         ProfileErrorCode errorCode = ProfileErrorCode.SELF_ACTION;
-        UUID id = target.getId();
 
+        when(profileRepository.existsById(authenticatedUserId)).thenReturn(true);
         when(sessionService.getAuthenticatedUserId()).thenReturn(authenticatedUserId);
-        when(profileRepository.findById(id)).thenReturn(Optional.of(authenticatedUserProfile));
 
         // act & assert
-        var ex = assertThrows(ApplicationException.class, () -> profileInteractionService.follow(id));
+        var ex = assertThrows(ApplicationException.class, () -> profileInteractionService.follow(authenticatedUserId));
         assertThat(ex.getMessage()).isEqualTo(errorCode.formatMessage());
 
         verify(profileFollowRepository, never()).save(any(ProfileFollow.class));
@@ -101,9 +94,9 @@ class ProfileInteractionServiceTest {
     void follow_ThrowsApplicationException_WhenProfileByIdAlreadyFollowedByYou() {
         // arrange
         ProfileErrorCode errorCode = ProfileErrorCode.ALREADY_FOLLOWING;
-        UUID id = target.getId();
+        UUID id = UUID.randomUUID();
 
-        when(profileRepository.findById(id)).thenReturn(Optional.of(target));
+        when(profileRepository.existsById(id)).thenReturn(true);
         when(sessionService.getAuthenticatedUserId()).thenReturn(authenticatedUserId);
         when(profileFollowRepository.existsByFollowerIdAndFollowedId(authenticatedUserId, id)).thenReturn(true);
 
@@ -117,7 +110,7 @@ class ProfileInteractionServiceTest {
     @Test
     void unfollow_ReturnsVoid() {
         // arrange
-        UUID id = target.getId();
+        UUID id = UUID.randomUUID();
 
         when(sessionService.getAuthenticatedUserId()).thenReturn(authenticatedUserId);
 
