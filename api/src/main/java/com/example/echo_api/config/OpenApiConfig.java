bb9129da4dc93e2a9671e31377fbded25b009a68
Ben Allenden <benallenden@protonmail.com>
2025-09-27 17:10:22 +0100

    Add OpenApiConfig

---
+++ api/src/main/java/com/example/echo_api/config/OpenApiConfig.java
@@ -0,0 +1,17 @@
+package com.example.echo_api.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+// @formatter:off
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Echo API",
+        description = "A REST API implementation to serve the Echo client.",
+        version = "0.2.0" // x-release-please-version
+    )
+)
+@Configuration
+public class OpenApiConfig {}
