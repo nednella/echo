ceece02b9d17dbef5d8b39dc639c238003a0da96
Ben Allenden <benallenden@protonmail.com>
2025-09-30 18:08:11 +0100

    Add type libs/api/client.ts type exports

--- apps/client/src/libs/api/client.ts
+++ apps/client/src/libs/api/client.ts
@@ -1,11 +1,15 @@
 import createClient from "openapi-fetch"
 
-import type { paths } from "./api-spec"
+import type { components, paths } from "./v1"
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL
 
+// TODO: validate .env variables in one place
 if (!BASE_URL) {
     throw new Error("API base URL missing from .env file!")
 }
 
 export const client = createClient<paths>({ baseUrl: BASE_URL })
+
+export type { paths, operations } from "./v1"
+export type schemas = components["schemas"]
