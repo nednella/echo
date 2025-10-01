1c7ea12a608473b6271b1594fe41433d695114cb
Ben Allenden <benallenden@protonmail.com>
2025-10-01 21:17:44 +0100

    Add ESLint TanStack Router plugin

--- apps/client/eslint.config.js
+++ apps/client/eslint.config.js
@@ -1,5 +1,6 @@
 import js from "@eslint/js"
 import eslintPluginQuery from "@tanstack/eslint-plugin-query"
+import eslintPluginRouter from "@tanstack/eslint-plugin-router"
 import eslintConfigPrettier from "eslint-config-prettier"
 import reactHooks from "eslint-plugin-react-hooks"
 import reactRefresh from "eslint-plugin-react-refresh"
@@ -17,6 +18,7 @@ export default tseslint.config([
             tseslint.configs.recommended,
             eslintPluginUnicorn.configs.recommended,
             eslintPluginQuery.configs["flat/recommended"],
+            eslintPluginRouter.configs["flat/recommended"],
             reactHooks.configs["recommended-latest"],
             reactRefresh.configs.vite,
             eslintConfigPrettier // must be last
