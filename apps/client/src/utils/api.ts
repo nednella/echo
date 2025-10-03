7a147ecefd5540a53aec878c151e9c715b56f4d8
Ben Allenden <benallenden@protonmail.com>
2025-10-03 18:03:40 +0100

    Add client error middleware w/ error toast for network issues

--- apps/client/src/utils/api.ts
+++ apps/client/src/utils/api.ts
@@ -1,4 +1,6 @@
-import { createClient } from "@/libs/api"
+import { toast } from "sonner"
+
+import { type Middleware, createClient } from "@/libs/api"
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL
 
@@ -7,4 +9,19 @@ if (!BASE_URL) {
     throw new Error("API base URL missing from .env file!")
 }
 
-export const client = createClient(BASE_URL)
+const errorMiddleware: Middleware = {
+    onResponse: () => {
+        console.log("onResponse")
+    },
+    onError: () => {
+        toast.error("A network error occurred", { description: "Please try again later" })
+    }
+}
+
+function createClientWithMiddleware() {
+    const client = createClient(BASE_URL)
+    client.use(errorMiddleware)
+    return client
+}
+
+export const client = createClientWithMiddleware()
