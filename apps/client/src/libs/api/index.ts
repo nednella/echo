060c8e89630503ec3d6f2931ff8ebf5ebe54cf34
Ben Allenden <benallenden@protonmail.com>
2025-10-03 17:54:59 +0100

    Move openapi client instantiation to utils/api.ts

--- apps/client/src/libs/api/index.ts
+++ apps/client/src/libs/api/index.ts
@@ -1,15 +1,8 @@
-import createClient from "openapi-fetch"
+import createOpenApiClient from "openapi-fetch"
 
 import type { components, paths } from "./v1"
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL
-
-// TODO: validate .env variables in one place
-if (!BASE_URL) {
-    throw new Error("API base URL missing from .env file!")
-}
-
-export const client = createClient<paths>({ baseUrl: BASE_URL })
+export const createClient = (baseUrl: string) => createOpenApiClient<paths>({ baseUrl })
 
 export type { paths, operations } from "./v1"
 export type schemas = components["schemas"]
--- apps/client/src/utils/api.ts
+++ apps/client/src/utils/api.ts
@@ -1,5 +1,16 @@
 import { QueryClient } from "@tanstack/react-query"
 
+import { createClient } from "@/libs/api"
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
+
+// TODO: validate .env variables in one place
+if (!BASE_URL) {
+    throw new Error("API base URL missing from .env file!")
+}
+
+export const client = createClient(BASE_URL)
+
 export const queryClient = new QueryClient({
     defaultOptions: {}
 })
