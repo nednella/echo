54fb11068264260e234d0ddb0407464fcaf5b79f
Ben Allenden <benallenden@protonmail.com>
2025-10-03 19:01:33 +0100

    Add ApiException class export to libs/api

--- apps/client/src/libs/api/index.ts
+++ apps/client/src/libs/api/index.ts
@@ -4,6 +4,22 @@ import type { components, paths } from "./v1"
 
 export const createClient = (baseUrl: string) => createOpenApiClient<paths>({ baseUrl })
 
+export class ApiException extends Error {
+    public timestamp: string
+    public status: number
+    public path: string
+    public response: Response
+
+    constructor(error: schemas["ErrorResponse"], response: Response) {
+        super(error.message)
+        this.timestamp = error.timestamp
+        this.status = error.status
+        this.path = error.path
+        this.response = response
+        this.name = "ApiException"
+    }
+}
+
 export type { Middleware } from "openapi-fetch"
 export type { operations, paths } from "./v1"
 export type schemas = components["schemas"]
