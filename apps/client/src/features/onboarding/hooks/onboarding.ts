2c90e204dc3b631eaaf6a5b7821a2b18e751394c
Ben Allenden <benallenden@protonmail.com>
2025-10-04 02:24:55 +0100

    Add onboarding mutation API call w/ minimum 3s wait

---
+++ apps/client/src/features/onboarding/hooks/onboarding.ts
@@ -0,0 +1,17 @@
+import { mutationOptions } from "@tanstack/react-query"
+
+import { client } from "@/utils/api"
+import { delayWithPromise } from "@/utils/delay"
+
+export const onboardingMutation = () => {
+    return mutationOptions({
+        mutationFn: async () => {
+            // API call is forced to a minimum delay for UX
+            const [response] = await Promise.allSettled([client.POST("/v1/clerk/onboarding"), delayWithPromise(3000)])
+
+            // The mutation will resolve to success if the API call status is not checked & explicity thrown in cases of rejection
+            if (response.status === "rejected") throw response.reason
+            return response.value.data
+        }
+    })
+}
