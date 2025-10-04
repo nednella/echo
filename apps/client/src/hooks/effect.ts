1788a1bb86545419d53176fb2d49816a5ab7d63c
Ben Allenden <benallenden@protonmail.com>
2025-10-04 02:02:32 +0100

    Update useEffectOnce to address strict mode mounting issue

--- apps/client/src/hooks/effect.ts
+++ apps/client/src/hooks/effect.ts
@@ -1,16 +1,25 @@
 import { useEffect, useRef } from "react"
 
+/**
+ * Custom hook to ensure a useEffect hook is invoked once only, tracked with a ref.
+ *
+ * A small timeout is added to prevent double mutations in strict mode during development.
+ *
+ * See: https://github.com/TanStack/query/issues/5341
+ */
 export const useEffectOnce = (effect: React.EffectCallback) => {
     const didRun = useRef(false)
 
     useEffect(() => {
-        if (!didRun.current) {
-            didRun.current = true
+        const id = setTimeout(() => {
+            if (!didRun.current) {
+                didRun.current = true
 
-            const destructor = effect()
-            if (destructor) return destructor
-        }
+                const destructor = effect()
+                if (destructor) return destructor
+            }
+        }, 50)
 
-        return () => {}
+        return () => clearTimeout(id)
     }, [effect])
 }
