59abe2770f770d7c5b3909402c6340ccf82c8cb5
Ben Allenden <benallenden@protonmail.com>
2025-10-04 22:02:49 +0100

    Add onboarding page API logic

--- apps/client/src/routes/(onboarding)/onboarding.tsx
+++ apps/client/src/routes/(onboarding)/onboarding.tsx
@@ -1,6 +1,8 @@
-import { useUser } from "@clerk/clerk-react"
+import { useMutation } from "@tanstack/react-query"
 import { createFileRoute, redirect } from "@tanstack/react-router"
 
+import { onboardingMutation } from "@/features/onboarding/hooks/onboarding"
+import { useEffectOnce } from "@/hooks/effect"
 import { Page } from "@/libs/ui/page"
 import { isAuthenticated, isOnboarded } from "@/utils/auth"
 
@@ -24,13 +26,24 @@ export const Route = createFileRoute("/(onboarding)/onboarding")({
 })
 
 function OnboardingPage() {
-    const user = useUser()
+    const onboarding = useMutation(onboardingMutation())
 
-    console.log(user)
+    const { auth } = Route.useRouteContext()
+    const navigate = Route.useNavigate()
+    const { getToken } = auth
 
-    return (
-        <Page>
-            <p>onboarding page</p>
-        </Page>
-    )
+    useEffectOnce(() => {
+        onboarding.mutate()
+    })
+
+    /**
+     * Token should be force refreshed after onboarding completes, otherwise
+     * the user will be redirected back to /onboarding due to missing claims.
+     */
+    const handleOnboardingComplete = async () => {
+        await getToken({ skipCache: true })
+        navigate({ to: "/home", replace: true })
+    }
+
+    return <Page className="flex items-center justify-center">{/* TODO: loading animation component */}</Page>
 }
