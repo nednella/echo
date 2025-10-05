6c8c619d9ccdcabfcff97cc1226a47a17276d4fc
Ben Allenden <benallenden@protonmail.com>
2025-10-05 18:52:56 +0100

    Add spinner component

---
+++ apps/client/src/components/spinner.tsx
@@ -0,0 +1,11 @@
+import { ScaleLoader } from "react-spinners"
+
+export default function Spinner() {
+    return (
+        <ScaleLoader
+            color="#0891b2"
+            margin={1}
+            width={3}
+        />
+    )
+}
