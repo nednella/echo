e9e00844b33e3fde42f9c5f083c9dcccff2f7a13
Ben Allenden <benallenden@protonmail.com>
2025-10-07 18:30:46 +0100

    Add rounded & fill bools to button primitive

--- apps/client/src/libs/ui/button.tsx
+++ apps/client/src/libs/ui/button.tsx
@@ -21,8 +21,8 @@ const buttonVariants = cva(
             },
             size: {
                 default: "h-9 px-4 py-2 has-[>svg]:px-3",
-                sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-                lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+                sm: "h-8 gap-1.5 px-3 has-[>svg]:px-2.5",
+                lg: "h-10 px-6 has-[>svg]:px-4",
                 icon: "size-9",
                 "icon-sm": "size-8",
                 "icon-lg": "size-10"
@@ -35,22 +35,23 @@ const buttonVariants = cva(
     }
 )
 
+type ButtonProps = React.ComponentProps<"button"> &
+    VariantProps<typeof buttonVariants> & { asChild?: boolean; rounded?: boolean; fill?: boolean }
+
 function Button({
     className,
     variant,
     size,
     asChild = false,
+    rounded = false,
+    fill = false,
     ...props
-}: React.ComponentProps<"button"> &
-    VariantProps<typeof buttonVariants> & {
-        asChild?: boolean
-    }) {
+}: Readonly<ButtonProps>) {
     const Comp = asChild ? Slot : "button"
-
     return (
         <Comp
             data-slot="button"
-            className={cn(buttonVariants({ variant, size, className }))}
+            className={cn(buttonVariants({ variant, size }), className, rounded && "rounded-full", fill && "w-full")}
             {...props}
         />
     )
