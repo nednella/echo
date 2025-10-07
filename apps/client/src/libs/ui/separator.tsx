0dc0ab96b0005489f834728ade042b71afe693c8
Ben Allenden <benallenden@protonmail.com>
2025-10-07 20:01:27 +0100

    Add LabelledSeparator

--- apps/client/src/libs/ui/separator.tsx
+++ apps/client/src/libs/ui/separator.tsx
@@ -4,17 +4,14 @@ import * as SeparatorPrimitive from "@radix-ui/react-separator"
 
 import { cn } from "@/libs/utils"
 
-function Separator({
-    className,
-    orientation = "horizontal",
-    decorative = true,
-    ...props
-}: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
+type SeparatorProps = React.ComponentProps<typeof SeparatorPrimitive.Root>
+
+function Separator({ className, orientation = "horizontal", decorative = true, ...props }: Readonly<SeparatorProps>) {
     return (
         <SeparatorPrimitive.Root
             data-slot="separator"
-            decorative={decorative}
             orientation={orientation}
+            decorative={decorative}
             className={cn(
                 `bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full
                 data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px`,
@@ -25,4 +22,26 @@ function Separator({
     )
 }
 
-export { Separator }
+type LabelledSeparatorProps = React.ComponentProps<typeof SeparatorPrimitive.Root> & { label: string }
+
+function LabelledSeparator({ className, label, decorative = true, ...props }: Readonly<LabelledSeparatorProps>) {
+    return (
+        <div className={cn("flex items-center gap-2", className)}>
+            <SeparatorPrimitive.Root
+                data-slot="separator"
+                decorative={decorative}
+                className={"bg-border data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full"}
+                {...props}
+            />
+            <span className="text-sm select-none">{label}</span>
+            <SeparatorPrimitive.Root
+                data-slot="separator"
+                decorative={decorative}
+                className={"bg-border data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full"}
+                {...props}
+            />
+        </div>
+    )
+}
+
+export { Separator, LabelledSeparator }
