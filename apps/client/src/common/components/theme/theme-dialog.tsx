7da083a0986c6bb0eee9b79314d863c33004b920
Ben Allenden <benallenden@protonmail.com>
2025-10-17 02:24:52 +0100

    Finish off theme dialog

--- apps/client/src/common/components/theme/theme-dialog.tsx
+++ apps/client/src/common/components/theme/theme-dialog.tsx
@@ -1,8 +1,14 @@
 import { useThemeDialog } from "@/common/stores/theme-dialog.store"
+import { useThemeStore } from "@/libs/theme/theme.store"
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/libs/ui/components/dialog"
+import { Separator } from "@/libs/ui/components/separator"
+
+import { ThemeCard } from "./theme-card"
+import { options } from "./theme-card.config"
 
 export function ThemeDialog() {
     const { isOpen, onClose } = useThemeDialog()
+    const { theme: activeTheme, setTheme } = useThemeStore()
 
     const onChange = (open: boolean) => {
         if (!open) {
@@ -15,11 +21,27 @@ export function ThemeDialog() {
             open={isOpen}
             onOpenChange={onChange}
         >
-            <DialogContent>
+            <DialogContent className="">
                 <DialogHeader>
-                    <DialogTitle>Theme Dialog</DialogTitle>
-                    <DialogDescription>Description</DialogDescription>
+                    <DialogTitle>Appearance</DialogTitle>
+                    <DialogDescription>Change how the application looks in your browser</DialogDescription>
                 </DialogHeader>
+                <Separator />
+                <div>
+                    <DialogTitle className="text-sm">Interface theme</DialogTitle>
+                    <DialogDescription>Select your UI theme</DialogDescription>
+                </div>
+                <div className="grid grid-cols-3 gap-4">
+                    {options.map((option) => (
+                        <ThemeCard
+                            key={option.theme}
+                            active={activeTheme === option.theme}
+                            label={option.label}
+                            src={option.src}
+                            onClick={() => setTheme(option.theme)}
+                        />
+                    ))}
+                </div>
             </DialogContent>
         </Dialog>
     )
