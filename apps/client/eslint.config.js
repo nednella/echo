446292fad5a5243f77c1d1725486adb6b86d5a15
Ben Allenden <benallenden@protonmail.com>
2025-10-17 16:46:19 +0100

    Configure import plugin to enforce unidirectional codebase and forbid cross-feature importing, as-per Bulletproof React

--- apps/client/eslint.config.js
+++ apps/client/eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js"
 import eslintPluginQuery from "@tanstack/eslint-plugin-query"
 import eslintPluginRouter from "@tanstack/eslint-plugin-router"
 import eslintConfigPrettier from "eslint-config-prettier"
+import eslintPluginImport from "eslint-plugin-import"
 import reactHooks from "eslint-plugin-react-hooks"
 import reactRefresh from "eslint-plugin-react-refresh"
 import eslintPluginUnicorn from "eslint-plugin-unicorn"
@@ -17,6 +18,8 @@ export default tseslint.config([
             js.configs.recommended,
             tseslint.configs.recommended,
             eslintPluginUnicorn.configs.recommended,
+            eslintPluginImport.flatConfigs.recommended,
+            eslintPluginImport.flatConfigs.typescript,
             eslintPluginQuery.configs["flat/recommended"],
             eslintPluginRouter.configs["flat/recommended"],
             reactHooks.configs["recommended-latest"],
@@ -30,6 +33,9 @@ export default tseslint.config([
         settings: {
             react: {
                 version: "detect"
+            },
+            "import/resolver": {
+                typescript: true
             }
         },
         rules: {
@@ -41,9 +47,63 @@ export default tseslint.config([
                         props: true,
                         Env: true,
                         env: true,
-                        utils: true,
-                        Utils: true
+                        Utils: true,
+                        utils: true
                     }
+                }
+            ],
+            "import/no-unresolved": "off", // does not support Vite natively, see: https://github.com/import-js/eslint-plugin-import/blob/v2.32.0/docs/rules/no-unresolved.md#when-not-to-use-it\
+            "import/no-default-export": "error", // disable default exporting
+            "import/no-restricted-paths": [
+                "error",
+                {
+                    basePath: "./src",
+                    zones: [
+                        // target — director(y/ies) under scrutiny
+                        // from — director(y/ies) that target(s) cannot depend on
+
+                        // enforce unidirectional codebase (see: https://github.com/alan2207/bulletproof-react/blob/master/docs/project-structure.md)
+                        {
+                            // 1) shared layers should not depend on higher-level application layers
+
+                            target: ["common", "libs"],
+                            from: ["features", "routes"],
+                            message: "❌ common/libs are shared layers — they should not import from features/routes!"
+                        },
+                        {
+                            // 2) features should not depend on routes
+
+                            target: "features",
+                            from: "routes",
+                            message: "❌ features are domain-specific modules — they should not import from routes!"
+                        },
+
+                        // disable cross-feature imports
+                        {
+                            target: "features/auth",
+                            from: "features",
+                            except: ["auth"],
+                            message: "❌ features are independent modules — they should not import from other features!"
+                        },
+                        {
+                            target: "features/landing",
+                            from: "features",
+                            except: ["landing"],
+                            message: "❌ features are independent modules — they should not import from other features!"
+                        },
+                        {
+                            target: "features/onboarding",
+                            from: "features",
+                            except: ["onboarding"],
+                            message: "❌ features are independent modules — they should not import from other features!"
+                        },
+                        {
+                            target: "features/settings",
+                            from: "features",
+                            except: ["settings"],
+                            message: "❌ features are independent modules — they should not import from other features!"
+                        }
+                    ]
                 }
             ]
         }
