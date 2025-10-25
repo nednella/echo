40dfd55a12249cf6f1b6b8615737be5bb19c5ae5
nednella <19449811+nednella@users.noreply.github.com>
2025-10-25 20:09:49 +0100

    Update fetch_profile SQL fn implementation

---
+++ apps/api/src/main/resources/db/repeatable/R__func_fetch_profile_by_username.sql
@@ -0,0 +1,25 @@
+CREATE OR REPLACE FUNCTION fetch_profile_by_username (
+    p_username VARCHAR,
+    p_viewer_id UUID
+)
+RETURNS TABLE (
+    id                 UUID,
+    username           VARCHAR(255),
+    name               VARCHAR(50),
+    bio                VARCHAR(160),
+    location           VARCHAR(30),
+    image_url          VARCHAR(255),
+    created_at         TIMESTAMPTZ,
+    follower_count     BIGINT,
+    following_count    BIGINT,
+    post_count         BIGINT,
+    media_count        BIGINT,
+    rel_is_self        BOOLEAN,
+    rel_following      BOOLEAN,
+    rel_followed_by    BOOLEAN
+)
+AS
+$$
+    SELECT * FROM profiles_with_context_and_viewer_v1(ARRAY(SELECT id FROM profiles WHERE username = p_username), p_viewer_id)
+$$
+LANGUAGE SQL
