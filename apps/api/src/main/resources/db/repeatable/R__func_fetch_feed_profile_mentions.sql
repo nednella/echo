90e705c939920896d195a81548a9a1844b9b09fd
nednella <19449811+nednella@users.noreply.github.com>
2025-11-03 16:40:14 +0000

    deduplicate profile mentions by selecting DISTINCT on post ID

--- apps/api/src/main/resources/db/repeatable/R__func_fetch_feed_profile_mentions.sql
+++ apps/api/src/main/resources/db/repeatable/R__func_fetch_feed_profile_mentions.sql
@@ -38,12 +38,12 @@ RETURNS TABLE (
 AS
 $$
     WITH profile_mentions AS (
-        SELECT
+        SELECT DISTINCT ON (p.id)
             p.id
         FROM posts p
         INNER JOIN post_entities pe ON pe.post_id = p.id AND pe.entity_type = 'MENTION'
-        INNER JOIN profiles pr ON pr.id = p_profile_id AND pr.username = pe.text
-        ORDER BY p.created_at DESC
+        INNER JOIN profiles pr ON pr.id = p_profile_id AND pr.username = pe.text -- fix: lower()
+        ORDER BY p.id, p.created_at DESC
         OFFSET p_offset
         LIMIT p_limit
     )
