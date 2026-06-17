08c5a58bff87f1df5eb4566fa187b0847ffac9c2
nednella <119449811+nednella@users.noreply.github.com>
2026-06-17 21:30:04 +0100

    refactor(client): replace Feed with generic PostList

--- apps/client/src/routes/(app)/home/feed.tsx
+++ apps/client/src/routes/(app)/home/feed.tsx
@@ -1,9 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router"
 
 import { feedInfiniteQueryOptions } from "@/features/feed/api/options"
-import { Feed } from "@/features/feed/components/feed"
+import { PostList } from "@/features/post/components/post-list"
 
 export const Route = createFileRoute("/(app)/home/feed")({
     loader: ({ context: { queryClient } }) => queryClient.ensureInfiniteQueryData(feedInfiniteQueryOptions("homepage")),
-    component: () => <Feed feed="homepage" />
+    component: () => <PostList options={feedInfiniteQueryOptions("homepage")} />
 })
